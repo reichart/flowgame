440e7963144de8e761f731f77ff9f8885068751f
huebnerc <huebnerc@1012749a-39bf-11de-9d05-31133e6853b1>
2009-05-13 22:16:57 +0000

    first try of tunnel with automatic movement in z-axis direction (realised with behaviors)

---
+++ trunk/src/j3DGame/testChristopher/ForwardNavigatorBehavior.java
@@ -0,0 +1,32 @@
+package j3DGame.testChristopher;
+
+import java.util.Enumeration;
+
+import javax.media.j3d.Behavior;
+import javax.media.j3d.Transform3D;
+import javax.media.j3d.TransformGroup;
+import javax.media.j3d.WakeupOnElapsedFrames;
+
+import com.sun.j3d.utils.behaviors.keyboard.KeyNavigator;
+
+public class ForwardNavigatorBehavior extends Behavior{
+
+	private WakeupOnElapsedFrames wakeEvent = new WakeupOnElapsedFrames(0);
+	private ForwardNavigator forwardNavigator;
+	
+    public ForwardNavigatorBehavior(TransformGroup targetTG) {
+    	this.forwardNavigator = new ForwardNavigator(targetTG);
+        }
+	
+	@Override
+	public void initialize() {
+		this.wakeupOn(wakeEvent);
+	}
+
+	@Override
+	public void processStimulus(Enumeration criteria) {
+		forwardNavigator.integrateTransformChanges();
+		wakeupOn(wakeEvent);
+	}
+	
+}
