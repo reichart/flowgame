62fccc43575f5a1914c11a3634ece3ffa96479dd
barbara.koehler <barbara.koehler@1012749a-39bf-11de-9d05-31133e6853b1>
2009-05-17 16:25:22 +0000

    tunnel now rotates around its own axis

--- trunk/src/j3DGame/testChristopher/Tunnel.java
+++ trunk/src/j3DGame/testChristopher/Tunnel.java
@@ -1,15 +1,15 @@
 package j3DGame.testChristopher;
 
+import javax.media.j3d.Alpha;
 import javax.media.j3d.Appearance;
 import javax.media.j3d.BoundingBox;
-import javax.media.j3d.ColoringAttributes;
+import javax.media.j3d.RotationInterpolator;
 import javax.media.j3d.Shape3D;
 import javax.media.j3d.Texture;
 import javax.media.j3d.TextureAttributes;
 import javax.media.j3d.Transform3D;
 import javax.media.j3d.TransformGroup;
 import javax.media.j3d.TransparencyAttributes;
-import javax.vecmath.Color3f;
 import javax.vecmath.Point3d;
 import javax.vecmath.Vector3f;
 
@@ -40,38 +40,48 @@ public class Tunnel extends TransformGroup {
 		// BoundingSphere tunnelPartBounds = new BoundingSphere(new Point3d(0.0,
 		// 0.0, 0.0), tunnelPartLength / 2);
 
-		final float q = 1.0f / TUNNEL_PARTS;
-
 		for (int i = 0; i < TUNNEL_PARTS; i++) {
 			final Shape3D tunnelShape = (Shape3D) cyl.getShape(Cylinder.BODY).cloneNode(true);
 			tunnelShape.setBoundsAutoCompute(false);
 			tunnelShape.setBounds(tunnelPartBounds);
 
 			tunnelShape.setAppearance(appearance);
-
-			final TransformGroup tunnelRotationGroup = new TransformGroup(rotation);
+			
+			//create a Rotation group, that includes the tunnel and the rotation group for Animation
+			final TransformGroup tunnelRotationGroup = new TransformGroup();
+			tunnelRotationGroup.setCapability(TransformGroup.ALLOW_TRANSFORM_WRITE);
+			
+			final RotationInterpolator rotInt = new RotationInterpolator(new Alpha(-1, 10000), tunnelRotationGroup);
+			rotInt.setSchedulingBounds(GameApplet.WORLD_BOUNDS);
+			
+			tunnelRotationGroup.addChild(rotInt);
+			tunnelRotationGroup.addChild(tunnelShape);
+			
+			//lay cylinder down, so that it appears as tunnel
+			final TransformGroup layTunnel = new TransformGroup(rotation);
+			layTunnel.addChild(tunnelRotationGroup);
+			
 			final TransformGroup tunnelTranslationGroup = new TransformGroup();
-
 			tunnelTranslationGroup.setCapability(TransformGroup.ALLOW_TRANSFORM_WRITE);
 			tunnelTranslationGroup.setCapability(TransformGroup.ALLOW_TRANSFORM_READ);
-
+			tunnelTranslationGroup.addChild(layTunnel);
+			
 			final Transform3D partTrans = new Transform3D();
 			partTrans.set(new Vector3f(0.0f, 0.0f, -(TUNNEL_LENGTH * i)));
 			final Transform3D part = new Transform3D();
 			tunnelTranslationGroup.getTransform(part);
 			part.mul(partTrans);
 			tunnelTranslationGroup.setTransform(part);
-			final TunnelPartMoveBehavior reuse = new TunnelPartMoveBehavior(tunnelShape, tunnelTranslationGroup,
+			final TunnelPartMoveBehavior reUseBevahior = new TunnelPartMoveBehavior(tunnelShape, tunnelTranslationGroup,
 					TUNNEL_LENGTH, TUNNEL_PARTS);
-			reuse.setSchedulingBounds(GameApplet.WORLD_BOUNDS);
-			tunnelTranslationGroup.addChild(reuse);
-			tunnelTranslationGroup.addChild(tunnelRotationGroup);
-			tunnelRotationGroup.addChild(tunnelShape);
+			reUseBevahior.setSchedulingBounds(GameApplet.WORLD_BOUNDS);
+			layTunnel.addChild(reUseBevahior);
+			
 			addChild(tunnelTranslationGroup);
 		}
 		final ForwardNavigatorBehavior fwdNav = new ForwardNavigatorBehavior(this);
 		fwdNav.setSchedulingBounds(GameApplet.WORLD_BOUNDS);
-
+		
 		addChild(fwdNav);
 	}
 
