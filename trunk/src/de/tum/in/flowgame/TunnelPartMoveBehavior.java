a9ede2151848aa99f1474e0badedbf7bcf693a28
google@vxart.de <google@vxart.de@1012749a-39bf-11de-9d05-31133e6853b1>
2009-05-30 22:47:10 +0000

    removed dead code

--- trunk/src/de/tum/in/flowgame/TunnelPartMoveBehavior.java
+++ trunk/src/de/tum/in/flowgame/TunnelPartMoveBehavior.java
@@ -5,44 +5,28 @@ import java.util.Enumeration;
 import javax.media.j3d.Behavior;
 import javax.media.j3d.Shape3D;
 import javax.media.j3d.TransformGroup;
-import javax.media.j3d.WakeupCondition;
 import javax.media.j3d.WakeupCriterion;
-import javax.media.j3d.WakeupOnElapsedFrames;
 import javax.media.j3d.WakeupOnViewPlatformExit;
-import javax.media.j3d.WakeupOr;
 
 public class TunnelPartMoveBehavior extends Behavior {
 
-	private WakeupCriterion wakeEvent;
-	private WakeupCriterion we2;
-	private WakeupCriterion[] warray = new WakeupCriterion[2];
-	private WakeupCondition w;
-	private TunnelPartMover tunnelPartMover;
-	private Shape3D element;
-	private TransformGroup tg;
+	private final WakeupCriterion wakeEvent;
+	private final TunnelPartMover tunnelPartMover;
 
-	public TunnelPartMoveBehavior(Shape3D element, TransformGroup targetTG, double zDist,
-			int parts) {
+	public TunnelPartMoveBehavior(final Shape3D element, final TransformGroup targetTG, final double zDist,
+			final int parts) {
 		this.tunnelPartMover = new TunnelPartMover(targetTG, zDist, parts);
 		this.wakeEvent = new WakeupOnViewPlatformExit(element.getBounds());
-		this.we2 = new WakeupOnElapsedFrames(0);
-		this.warray[0] = this.wakeEvent;
-		this.warray[1] = this.we2;
-		this.w = new WakeupOr(this.warray);
-		this.element = element;
-		this.tg = targetTG;
 	}
 
 	@Override
 	public void initialize() {
-		// Transform3D blub = new Transform3D();
-		// test.getLocalToVworld(blub);
-		// System.out.println(blub);
 		this.wakeupOn(wakeEvent);
 	}
 
 	@Override
-	public void processStimulus(Enumeration criteria) {
+	@SuppressWarnings("unchecked")
+	public void processStimulus(final Enumeration criteria) {
 		this.tunnelPartMover.integrateTransformChanges();
 		this.wakeupOn(wakeEvent);
 	}
