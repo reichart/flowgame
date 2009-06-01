599ae36442127e11c064add62a1d3f60054e7f1e
google@vxart.de <google@vxart.de@1012749a-39bf-11de-9d05-31133e6853b1>
2009-06-01 10:58:11 +0000

    new method printRootPath

--- src/de/tum/in/flowgame/Utils.java
+++ src/de/tum/in/flowgame/Utils.java
@@ -1,8 +1,21 @@
 package de.tum.in.flowgame;
 
+import javax.media.j3d.Node;
+
 public class Utils {
 
-	public static <T> T[] asArray(T... t) {
+	public static <T> T[] asArray(final T... t) {
 		return t;
+	}
+
+	public void printRootPath(final Node node) {
+		if (node == null) {
+			System.out.println();
+			return;
+		} else {
+			final String instance = node.getClass().getSimpleName() + "@" + Integer.toHexString(node.hashCode());
+			System.out.print(" > " + instance);
+			printRootPath(node.getParent());
+		}
 	}
 }
