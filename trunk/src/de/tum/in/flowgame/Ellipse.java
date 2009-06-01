81ac5593dfcf374a1bf480c846711d0b02c99614
google@vxart.de <google@vxart.de@1012749a-39bf-11de-9d05-31133e6853b1>
2009-06-01 22:34:25 +0000

    precalculate pow's in Ellipse

--- trunk/src/de/tum/in/flowgame/Ellipse.java
+++ trunk/src/de/tum/in/flowgame/Ellipse.java
@@ -1,14 +1,31 @@
 package de.tum.in.flowgame;
 
 public class Ellipse {
+
 	public static final float ELLIPSE_A = 1.7f;
 	public static final float ELLIPSE_B = 0.7f;
 
-	public final static double getYOnPosition(double x) {
-		if (x > ELLIPSE_A) {
+	private final static Ellipse ellipse = new Ellipse(ELLIPSE_A, ELLIPSE_B);
+
+	private final double aSquared;
+	private final double bSquared;
+
+	public static double getYOnPosition(final double x) {
+		return ellipse.getY(x);
+	}
+
+	public Ellipse(final double a, final double b) {
+		this.aSquared = Math.pow(a, 2);
+		this.bSquared = Math.pow(b, 2);
+	}
+
+	public double getY(final double x) {
+		final double xSquared = Math.pow(x, 2);
+
+		if (xSquared > aSquared) {
 			return -1;
 		} else {
-			return Math.sqrt((1 - (Math.pow(x, 2) / Math.pow(ELLIPSE_A, 2))) * Math.pow(ELLIPSE_B, 2));
+			return Math.sqrt((1 - (xSquared / aSquared)) * bSquared);
 		}
 	}
 
