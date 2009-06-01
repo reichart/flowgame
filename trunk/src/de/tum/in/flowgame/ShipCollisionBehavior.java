19409050af5463dadd8e4d209ca9fc9e5680990a
google@vxart.de <google@vxart.de@1012749a-39bf-11de-9d05-31133e6853b1>
2009-06-01 11:01:39 +0000

    cleaned up

--- trunk/src/de/tum/in/flowgame/ShipCollisionBehavior.java
+++ trunk/src/de/tum/in/flowgame/ShipCollisionBehavior.java
@@ -11,58 +11,35 @@ import javax.media.j3d.WakeupOnCollisionMovement;
 import javax.media.j3d.WakeupOr;
 
 public class ShipCollisionBehavior extends Behavior {
-	private WakeupOr wakeUp;
-	Node node;
-	private final GameLogic collisionCounter;
-	
-	
-	public ShipCollisionBehavior(Node node, GameLogic collisionCounter) {
-		this.node = node;
-		this.collisionCounter = collisionCounter;
-		WakeupOnCollisionEntry w1 = new WakeupOnCollisionEntry(node, WakeupOnCollisionEntry.USE_BOUNDS);
-		WakeupOnCollisionExit w2 = new WakeupOnCollisionExit(this.node);
-		WakeupOnCollisionMovement w3 = new WakeupOnCollisionMovement(this.node);
-		WakeupCriterion[] wakeupCriterion = {w1, w2, w3};
-		wakeUp = new WakeupOr(wakeupCriterion);
+
+	private final WakeupOr wakeUp;
+	private final GameLogic logic;
+
+	public ShipCollisionBehavior(final Node node, final GameLogic logic) {
+		final WakeupOnCollisionEntry w1 = new WakeupOnCollisionEntry(node, WakeupOnCollisionEntry.USE_BOUNDS);
+		final WakeupOnCollisionExit w2 = new WakeupOnCollisionExit(node);
+		final WakeupOnCollisionMovement w3 = new WakeupOnCollisionMovement(node);
+		final WakeupCriterion[] wakeupCriterion = { w1, w2, w3 };
+
+		this.wakeUp = new WakeupOr(wakeupCriterion);
+		this.logic = logic;
 	}
 
 	@Override
 	public void initialize() {
-		System.out.println("initCollide");
 		wakeupOn(wakeUp);
 	}
 
 	@Override
 	@SuppressWarnings("unchecked")
-	public void processStimulus(Enumeration criteria) {
+	public void processStimulus(final Enumeration criteria) {
 		while (criteria.hasMoreElements()) {
-			Object element = criteria.nextElement();
+			final Object element = criteria.nextElement();
 			if (element instanceof WakeupOnCollisionEntry) {
-				WakeupOnCollisionEntry collision = (WakeupOnCollisionEntry) element;
-				Node collidable = collision.getTriggeringPath().getObject();
-				System.out.println("collideEnter with " + collidable.getParent().getUserData());
-				collisionCounter.add(collidable);
-				System.out.println(collisionCounter.getPoints());				
-			}
-			if (element instanceof WakeupOnCollisionExit) {
-				System.out.println("collideExit");
-			}
-			if (element instanceof WakeupOnCollisionMovement) {
-//				WakeupOnCollisionMovement wocm = (WakeupOnCollisionMovement)element;
-//				Node leaf = wocm.getTriggeringPath().getObject();
-//				System.out.println(toString(node) + " collides with " + leaf + "(" + (leaf == null ? null : leaf.getUserData()) + ")");
-//				while (leaf.getParent() != null) {
-//					System.out.print(" < " + toString(leaf));
-//					leaf = leaf.getParent();
-//				}
-//				System.out.println();
+				final WakeupOnCollisionEntry collision = (WakeupOnCollisionEntry) element;
+				logic.add(collision.getTriggeringPath().getObject());
 			}
 		}
 		wakeupOn(wakeUp);
 	}
-
-	private String toString(final Object n) {
-		return n.getClass().getSimpleName() + "@" + Integer.toHexString(n.hashCode());
-	}
-
 }
