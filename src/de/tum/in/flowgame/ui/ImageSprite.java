8220aed9228831ff5767d1586b99a20c5c7cafb2
google@vxart.de <google@vxart.de@1012749a-39bf-11de-9d05-31133e6853b1>
2009-06-13 13:50:01 +0000

    SVG icons and scalable UI

---
+++ src/de/tum/in/flowgame/ui/ImageSprite.java
@@ -0,0 +1,26 @@
+package de.tum.in.flowgame.ui;
+
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+public class ImageSprite implements Sprite {
+
+	private final BufferedImage image;
+
+	public ImageSprite(final String resource) throws IOException {
+		this.image = ImageIO.read(getClass().getResource(resource));
+	}
+
+	@Override
+	public void render(final Graphics2D g, final int x, final int y) {
+		g.drawImage(image, x, y, null);
+	}
+
+	@Override
+	public void render(final Graphics2D g, final int x, final int y, final int w, final int h) {
+		g.drawImage(image, x, y, w, h, null);
+	}
+}
