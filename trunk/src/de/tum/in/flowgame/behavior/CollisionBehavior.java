c15e9ca0f443828c822d1ca7de9f13c0f47b5d0c
sevi.voest <sevi.voest@1012749a-39bf-11de-9d05-31133e6853b1>
2009-07-05 15:45:19 +0000

    collision detection improved

--- trunk/src/de/tum/in/flowgame/behavior/CollisionBehavior.java
+++ trunk/src/de/tum/in/flowgame/behavior/CollisionBehavior.java
@@ -4,10 +4,10 @@ import java.util.Enumeration;
 
 import javax.media.j3d.Behavior;
 import javax.media.j3d.BoundingBox;
+import javax.media.j3d.BoundingSphere;
 import javax.media.j3d.Bounds;
 import javax.media.j3d.BranchGroup;
 import javax.media.j3d.Node;
-import javax.media.j3d.Transform3D;
 import javax.media.j3d.WakeupCondition;
 import javax.media.j3d.WakeupOnElapsedFrames;
 import javax.vecmath.Vector3d;
@@ -18,12 +18,12 @@ import de.tum.in.flowgame.Ship;
 public class CollisionBehavior extends Behavior {
 	private final WakeupCondition condition;
 	BranchGroup branchGroup;
-	
+
 	public CollisionBehavior(BranchGroup collidables) {
 		condition = new WakeupOnElapsedFrames(0);
 		branchGroup = collidables;
 	}
-	
+
 	@Override
 	public void initialize() {
 		wakeupOn(condition);
@@ -38,40 +38,49 @@ public class CollisionBehavior extends Behavior {
 			if (child instanceof Ship) {
 				ship = (Ship) child;
 				Vector3d vec = ship.getVector3dtShipPos();
-//				System.out.println("Ship: " + ship.getXPos() + ", " + ship.getYPos());
+				// System.out.println("Ship: " + ship.getXPos() + ", " +
+				// ship.getYPos());
 			}
 		}
 		final double shipX = ship.getVector3dtShipPos().getX();
 		final double shipY = ship.getVector3dtShipPos().getY();
 		final double shipZ = ship.getVector3dtShipPos().getZ();
-		final Bounds shipBounds = ship.getBounds();
-		
-		BoundingBox newBounds = new BoundingBox();
-		
+
 		while (children.hasMoreElements()) {
 			Node child = children.nextElement();
 			if (child instanceof Collidable) {
 				Collidable collidable = (Collidable) child;
-				
+
 				final double zPos = collidable.getZPos();
 				final double xPos = collidable.getXPos();
 				final double yPos = collidable.getYPos();
-				
+
 				final double oldz = collidable.getOldZPos();
-				final Bounds collidableBounds = collidable.getBounds();
-				
-				Transform3D trans = new Transform3D();
-				
-				newBounds.transform(shipBounds, trans);
-				
-				
-//				double xdiff = xPos-shipX;
-//				double ydiff = yPos-shipY;
-//				double zdiff = zPos-shipZ;
-//				
-//				System.out.println("Difference:"+ xdiff + ", "+ydiff+ ", "+zdiff);
-//				
-//				System.out.println(collidable.getXPos() + ", " + collidable.getYPos() + ", " + zPos);
+
+				if (zPos > 0 && oldz <= 0) {
+
+					final Bounds collidableBounds = collidable.getBounds();
+
+					BoundingBox newBounds = new BoundingBox();
+					newBounds.setLower(shipX, shipY, oldz);
+					newBounds.setUpper(shipX, shipY, zPos);
+
+					if (collidableBounds.intersect(newBounds)) {
+						System.out.println("COLLISION! BAAAAM!" + oldz + ", "
+								+ zPos);
+
+					}
+				}
+
+				// double xdiff = xPos-shipX;
+				// double ydiff = yPos-shipY;
+				// double zdiff = zPos-shipZ;
+				//				
+				// System.out.println("Difference:"+ xdiff + ", "+ydiff+ ",
+				// "+zdiff);
+				//				
+				// System.out.println(collidable.getXPos() + ", " +
+				// collidable.getYPos() + ", " + zPos);
 				collidable.setOldZPos(zPos);
 			}
 		}
