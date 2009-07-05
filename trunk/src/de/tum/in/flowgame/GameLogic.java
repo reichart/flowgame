60585a43f7ad4b1d0faf8aacd4090cf11b2218aa
google@vxart.de <google@vxart.de@1012749a-39bf-11de-9d05-31133e6853b1>
2009-07-05 16:07:23 +0000

    - more advanced GameLogic integrated with GameRound from model - basic JMX support

--- trunk/src/de/tum/in/flowgame/GameLogic.java
+++ trunk/src/de/tum/in/flowgame/GameLogic.java
@@ -1,39 +1,93 @@
 package de.tum.in.flowgame;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.media.j3d.Node;
 
 import de.tum.in.flowgame.behavior.CreateCollidablesBehavior;
 import de.tum.in.flowgame.behavior.SpeedChangeBehavior;
+import de.tum.in.flowgame.model.GameRound;
 
-public class GameLogic {
-	
-	public enum Item{FUELCAN, ASTEROID};
+public class GameLogic implements Runnable, GameLogicMBean {
 
-	private Tunnel tunnel;
-	
-	private int fuel;
+	public enum Item {
+		FUELCAN(Sounds.FUELCAN), ASTEROID(Sounds.ASTEROID);
+
+		private final Sounds snd;
+
+		private Item(final Sounds snd) {
+			this.snd = snd;
+		}
+
+		void play() {
+			snd.play();
+		}
+	};
+
+	public final static int MAX_ASTEROIDS = 10;
+	public final static int MAX_FUEL = 10;
+
+	private final List<GameListener> listeners;
+
+	private final Tunnel tunnel;
+
+	private volatile int fuel;
 	private int asteroids;
-	
-	private CreateCollidablesBehavior ccb;
-	
-	public GameLogic(CreateCollidablesBehavior ccb, Tunnel tunnel) {
+
+	private final CreateCollidablesBehavior ccb;
+
+	public GameLogic(final CreateCollidablesBehavior ccb, final Tunnel tunnel) {
+		this.listeners = new ArrayList<GameListener>();
 		this.ccb = ccb;
 		this.tunnel = tunnel;
-		SpeedChangeBehavior speedChange = new SpeedChangeBehavior(this.tunnel.getFwdNav());
+
+		final SpeedChangeBehavior speedChange = new SpeedChangeBehavior(this.tunnel.getFwdNav());
 		speedChange.setSchedulingBounds(Game3D.WORLD_BOUNDS);
 		tunnel.addChild(speedChange);
+
+		final GameRound round = new GameRound();
+		addListener(round);
+
+		Utils.export(this);
 	}
 
-	public void add(final Node node) {
-		Object userData = node.getUserData();
-		System.out.println(userData);
-		if (Item.FUELCAN.toString().equals(userData)) {
+	public void collide(final Node node) {
+		final Item item = (Item) node.getUserData();
+
+		fireCollided(item);
+
+		switch (item) {
+		case FUELCAN:
 			fuel++;
-			Sounds.FUELCAN.play();
-		} else if (Item.ASTEROID.toString().equals(userData)) {
+			break;
+		case ASTEROID:
 			asteroids++;
-			Sounds.ASTEROID.play();
+			break;
 		}
+
+		item.play();
+	}
+
+	@Override
+	public void run() {
+		fireGameStarted();
+
+		while (asteroids < MAX_ASTEROIDS && fuel > 0) {
+			try {
+				Thread.sleep(1000);
+			} catch (final InterruptedException ex) {
+				// ignore
+			}
+
+			fuel--;
+
+			if (fuel == 0) {
+				break;
+			}
+		}
+
+		fireGameStopped();
 	}
 
 	public int getFuel() {
@@ -43,12 +97,40 @@ public class GameLogic {
 	public int getAsteroids() {
 		return asteroids;
 	}
-	
+
 	public double getTunnelSpeed() {
 		return this.tunnel.getFwdNav().getSpeed();
 	}
-	
-	public void setTunnelSpeed(double tunnelSpeed){
+
+	public void setTunnelSpeed(final double tunnelSpeed) {
 		this.tunnel.getFwdNav().setSpeed(tunnelSpeed);
+	}
+
+	private void addListener(final GameListener listener) {
+		this.listeners.add(listener);
+	}
+
+	private void fireGameStarted() {
+		for (final GameListener listener : listeners) {
+			listener.gameStarted(this);
+		}
+	}
+
+	private void fireGamePaused() {
+		for (final GameListener listener : listeners) {
+			listener.gamePaused(this);
+		}
+	}
+
+	private void fireGameStopped() {
+		for (final GameListener listener : listeners) {
+			listener.gameStopped(this);
+		}
+	}
+
+	private void fireCollided(Item item) {
+		for (final GameListener listener : listeners) {
+			listener.collided(this, item);
+		}
 	}
 }
