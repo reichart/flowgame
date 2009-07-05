085513ade9f890a0adb1bdd05d9f3e6954929241
google@vxart.de <google@vxart.de@1012749a-39bf-11de-9d05-31133e6853b1>
2009-07-05 16:55:46 +0000

    cleanup

--- src/de/tum/in/flowgame/behavior/KeyShipBehavior.java
+++ src/de/tum/in/flowgame/behavior/KeyShipBehavior.java
@@ -13,11 +13,8 @@ import javax.media.j3d.WakeupOnAWTEvent;
 import javax.media.j3d.WakeupOnElapsedFrames;
 import javax.media.j3d.WakeupOr;
 import javax.vecmath.Matrix3f;
-import javax.vecmath.Point2d;
 import javax.vecmath.Point3d;
 import javax.vecmath.Vector3d;
-
-import com.sun.org.apache.xalan.internal.xsltc.cmdline.Transform;
 
 import de.tum.in.flowgame.Ship;
 import de.tum.in.flowgame.Tunnel;
@@ -33,7 +30,7 @@ public class KeyShipBehavior extends Behavior {
 	private final Transform3D trans = new Transform3D();
 
 	private final Vector3d mov = new Vector3d(0, 0, 0);
-	private static final double MAX_FOLLOWING_DIST = 2;
+//	private static final double MAX_FOLLOWING_DIST = 2;
 	private Vector3d a = new Vector3d();
 
 	private Vector3d leftAcc;
@@ -351,191 +348,191 @@ public class KeyShipBehavior extends Behavior {
 		
 	}
 
-	private void adaptMovementToTunnelRadius() {
-		int circleSection = outOfCircleSection(pos);
-		if (circleSection > 0) {
-			switch (circleSection) {
-			case 1:
-				if (mov.x > mov.y) {
-					if (pos.x <= MOV_RADIUS - Ship.INITIAL_SHIP_PLACEMENT_X) {
-						pos.y = allowedDistToCircleRadius(pos.x
-								+ Ship.INITIAL_SHIP_PLACEMENT_X, MOV_RADIUS)
-								- Ship.INITIAL_SHIP_PLACEMENT_Y;
-					} else {
-						pos.x = MOV_RADIUS - Ship.INITIAL_SHIP_PLACEMENT_X;
-						pos.y = 0 - Ship.INITIAL_SHIP_PLACEMENT_Y;
-					}
-				} else if (mov.y > mov.x) {
-					if (pos.y <= MOV_RADIUS - Ship.INITIAL_SHIP_PLACEMENT_Y) {
-						pos.x = allowedDistToCircleRadius(pos.y
-								+ Ship.INITIAL_SHIP_PLACEMENT_Y, MOV_RADIUS)
-								- Ship.INITIAL_SHIP_PLACEMENT_X;
-					} else {
-						pos.y = MOV_RADIUS - Ship.INITIAL_SHIP_PLACEMENT_Y;
-						pos.x = 0 - Ship.INITIAL_SHIP_PLACEMENT_X;
-					}
-				} else {
-					Point2d newPos = nearstPointOnCirce(pos, mov, circleSection);
-					pos.x = newPos.x;
-					pos.y = newPos.y;
-				}
-				break;
-			case 2:
-				if (-mov.x > mov.y) {
-					if (-pos.x <= MOV_RADIUS - Ship.INITIAL_SHIP_PLACEMENT_X) {
-						pos.y = allowedDistToCircleRadius(-pos.x
-								+ Ship.INITIAL_SHIP_PLACEMENT_X, MOV_RADIUS)
-								- Ship.INITIAL_SHIP_PLACEMENT_Y;
-					} else {
-						pos.x = -MOV_RADIUS - Ship.INITIAL_SHIP_PLACEMENT_X;
-						pos.y = 0 - Ship.INITIAL_SHIP_PLACEMENT_Y;
-					}
-				} else if (mov.y > -mov.x) {
-					if (pos.y <= MOV_RADIUS - Ship.INITIAL_SHIP_PLACEMENT_Y) {
-						pos.x = -allowedDistToCircleRadius(pos.y
-								+ Ship.INITIAL_SHIP_PLACEMENT_Y, MOV_RADIUS)
-								- Ship.INITIAL_SHIP_PLACEMENT_X;
-					} else {
-						pos.y = MOV_RADIUS - Ship.INITIAL_SHIP_PLACEMENT_Y;
-						pos.x = 0 + Ship.INITIAL_SHIP_PLACEMENT_X;
-					}
-				} else {
-					Point2d newPos = nearstPointOnCirce(pos, mov, circleSection);
-					pos.x = newPos.x;
-					pos.y = newPos.y;
-				}
-				break;
-			case 3:
-				if (mov.x < mov.y) {
-					if (pos.x >= -MOV_RADIUS - Ship.INITIAL_SHIP_PLACEMENT_X) {
-						pos.y = -allowedDistToCircleRadius(pos.x
-								+ Ship.INITIAL_SHIP_PLACEMENT_X, MOV_RADIUS)
-								- Ship.INITIAL_SHIP_PLACEMENT_Y;
-					} else {
-						pos.x = -MOV_RADIUS - Ship.INITIAL_SHIP_PLACEMENT_X;
-						pos.y = 0 + Ship.INITIAL_SHIP_PLACEMENT_Y;
-					}
-				} else if (mov.y < mov.x) {
-					if (pos.y >= -MOV_RADIUS - Ship.INITIAL_SHIP_PLACEMENT_Y) {
-						pos.x = -allowedDistToCircleRadius(pos.y
-								+ Ship.INITIAL_SHIP_PLACEMENT_Y, MOV_RADIUS)
-								- Ship.INITIAL_SHIP_PLACEMENT_X;
-					} else {
-						pos.y = -MOV_RADIUS - Ship.INITIAL_SHIP_PLACEMENT_Y;
-						pos.x = 0 + Ship.INITIAL_SHIP_PLACEMENT_X;
-					}
-				} else {
-					Point2d newPos = nearstPointOnCirce(pos, mov, circleSection);
-					pos.x = newPos.x;
-					pos.y = newPos.y;
-				}
-				break;
-			case 4:
-				if (mov.x > -mov.y) {
-					if (pos.x <= MOV_RADIUS - Ship.INITIAL_SHIP_PLACEMENT_X) {
-						pos.y = -allowedDistToCircleRadius(pos.x
-								+ Ship.INITIAL_SHIP_PLACEMENT_X, MOV_RADIUS)
-								- Ship.INITIAL_SHIP_PLACEMENT_Y;
-					} else {
-						pos.x = MOV_RADIUS - Ship.INITIAL_SHIP_PLACEMENT_X;
-						pos.y = 0 - Ship.INITIAL_SHIP_PLACEMENT_Y;
-					}
-				} else if (mov.y < mov.x) {
-					if (pos.y >= -MOV_RADIUS - Ship.INITIAL_SHIP_PLACEMENT_Y) {
-						pos.x = allowedDistToCircleRadius(pos.y
-								+ Ship.INITIAL_SHIP_PLACEMENT_Y, MOV_RADIUS)
-								- Ship.INITIAL_SHIP_PLACEMENT_X;
-					} else {
-						pos.y = -MOV_RADIUS - Ship.INITIAL_SHIP_PLACEMENT_Y;
-						pos.x = 0 - Ship.INITIAL_SHIP_PLACEMENT_X;
-					}
-				} else {
-					Point2d newPos = nearstPointOnCirce(pos, mov, circleSection);
-					pos.x = newPos.x;
-					pos.y = newPos.y;
-				}
-
-			}
-		}
-	}
-
-	private Point2d[] circleLineIntersection(double r, double xm, double ym,
-			double m, double n) {
-		double p = (2 * n * m - 2 * xm - 2 * m * ym - ym) / (1 + m * m) / 2;
-		double q = (xm * xm + n * n - 2 * n * ym - r * r) / (1 + m * m);
-		double numResults = p * p - q;
-		if (numResults < 0) {
-			return new Point2d[0];
-		}
-		double x1 = -p + Math.sqrt(p * p - q);
-		double y1 = n + m * x1;
-		if (numResults == 0) {
-			Point2d[] result = { new Point2d(x1, y1) };
-			return result;
-		}
-		double x2 = -p - Math.sqrt(p * p - q);
-		double y2 = n + m * x2;
-		Point2d[] result = { new Point2d(x1, y1), new Point2d(x2, y2) };
-		return result;
-	}
-
-	private Point2d nearstPointOnCirce(Vector3d pos, Vector3d mov, int section) {
-		if (mov.x == 0 & mov.y == 0)
-			return new Point2d(pos.x, pos.y);
-		double m = mov.y / mov.x;
-		double n = pos.y - m * pos.x;
-		Point2d[] intersections = circleLineIntersection(MOV_RADIUS,
-				-Ship.INITIAL_SHIP_PLACEMENT_X,
-				-Ship.INITIAL_SHIP_PLACEMENT_Y, m, n);
-		if (intersections.length < 2) {
-			return intersections[0];
-		} else {
-			double dist1 = Math.sqrt(Math.pow(pos.x - intersections[0].x, 2)
-					+ Math.pow(pos.y - intersections[0].y, 2));
-			double dist2 = Math.sqrt(Math.pow(pos.x - intersections[1].x, 2)
-					+ Math.pow(pos.y - intersections[1].y, 2));
-			if (dist1 < dist2) {
-				return intersections[0];
-			} else {
-				return intersections[1];
-			}
-		}
-	}
-
-	private int outOfCircleSection(Vector3d pos) {
-		double radius = Math.sqrt(Math.pow(
-				(pos.x + Ship.INITIAL_SHIP_PLACEMENT_X), 2)
-				+ Math.pow((pos.y + Ship.INITIAL_SHIP_PLACEMENT_Y), 2));
-		if (radius <= MOV_RADIUS) {
-			return 0;
-		}
-		if (pos.x >= 0 & pos.y > 0)
-			return 1;
-		if (pos.x < 0 & pos.y >= 0)
-			return 2;
-		if (pos.x <= 0 & pos.y < 0)
-			return 3;
-		return 4;
-	}
-
-	private Point3d shipToViewPosition(double v_x, double v_y) {
-		Point3d dp = new Point3d();
-		if (v_x < 0) {
-			dp.x = -v_x / leftVMax;
-		} else if (v_x > 0) {
-			dp.x = v_x / rightVMax;
-		}
-		if (v_y < 0) {
-			dp.y = -v_y / downVMax;
-		} else if (v_y > 0) {
-			dp.y = v_y / upVMax;
-		}
-		dp.scale(MAX_FOLLOWING_DIST);
-		if (dp.x != 0 | dp.y != 0) {
-			System.out.println("dp: " + dp);
-		}
-		return dp;
-	}
+//	private void adaptMovementToTunnelRadius() {
+//		int circleSection = outOfCircleSection(pos);
+//		if (circleSection > 0) {
+//			switch (circleSection) {
+//			case 1:
+//				if (mov.x > mov.y) {
+//					if (pos.x <= MOV_RADIUS - Ship.INITIAL_SHIP_PLACEMENT_X) {
+//						pos.y = allowedDistToCircleRadius(pos.x
+//								+ Ship.INITIAL_SHIP_PLACEMENT_X, MOV_RADIUS)
+//								- Ship.INITIAL_SHIP_PLACEMENT_Y;
+//					} else {
+//						pos.x = MOV_RADIUS - Ship.INITIAL_SHIP_PLACEMENT_X;
+//						pos.y = 0 - Ship.INITIAL_SHIP_PLACEMENT_Y;
+//					}
+//				} else if (mov.y > mov.x) {
+//					if (pos.y <= MOV_RADIUS - Ship.INITIAL_SHIP_PLACEMENT_Y) {
+//						pos.x = allowedDistToCircleRadius(pos.y
+//								+ Ship.INITIAL_SHIP_PLACEMENT_Y, MOV_RADIUS)
+//								- Ship.INITIAL_SHIP_PLACEMENT_X;
+//					} else {
+//						pos.y = MOV_RADIUS - Ship.INITIAL_SHIP_PLACEMENT_Y;
+//						pos.x = 0 - Ship.INITIAL_SHIP_PLACEMENT_X;
+//					}
+//				} else {
+//					Point2d newPos = nearstPointOnCirce(pos, mov, circleSection);
+//					pos.x = newPos.x;
+//					pos.y = newPos.y;
+//				}
+//				break;
+//			case 2:
+//				if (-mov.x > mov.y) {
+//					if (-pos.x <= MOV_RADIUS - Ship.INITIAL_SHIP_PLACEMENT_X) {
+//						pos.y = allowedDistToCircleRadius(-pos.x
+//								+ Ship.INITIAL_SHIP_PLACEMENT_X, MOV_RADIUS)
+//								- Ship.INITIAL_SHIP_PLACEMENT_Y;
+//					} else {
+//						pos.x = -MOV_RADIUS - Ship.INITIAL_SHIP_PLACEMENT_X;
+//						pos.y = 0 - Ship.INITIAL_SHIP_PLACEMENT_Y;
+//					}
+//				} else if (mov.y > -mov.x) {
+//					if (pos.y <= MOV_RADIUS - Ship.INITIAL_SHIP_PLACEMENT_Y) {
+//						pos.x = -allowedDistToCircleRadius(pos.y
+//								+ Ship.INITIAL_SHIP_PLACEMENT_Y, MOV_RADIUS)
+//								- Ship.INITIAL_SHIP_PLACEMENT_X;
+//					} else {
+//						pos.y = MOV_RADIUS - Ship.INITIAL_SHIP_PLACEMENT_Y;
+//						pos.x = 0 + Ship.INITIAL_SHIP_PLACEMENT_X;
+//					}
+//				} else {
+//					Point2d newPos = nearstPointOnCirce(pos, mov, circleSection);
+//					pos.x = newPos.x;
+//					pos.y = newPos.y;
+//				}
+//				break;
+//			case 3:
+//				if (mov.x < mov.y) {
+//					if (pos.x >= -MOV_RADIUS - Ship.INITIAL_SHIP_PLACEMENT_X) {
+//						pos.y = -allowedDistToCircleRadius(pos.x
+//								+ Ship.INITIAL_SHIP_PLACEMENT_X, MOV_RADIUS)
+//								- Ship.INITIAL_SHIP_PLACEMENT_Y;
+//					} else {
+//						pos.x = -MOV_RADIUS - Ship.INITIAL_SHIP_PLACEMENT_X;
+//						pos.y = 0 + Ship.INITIAL_SHIP_PLACEMENT_Y;
+//					}
+//				} else if (mov.y < mov.x) {
+//					if (pos.y >= -MOV_RADIUS - Ship.INITIAL_SHIP_PLACEMENT_Y) {
+//						pos.x = -allowedDistToCircleRadius(pos.y
+//								+ Ship.INITIAL_SHIP_PLACEMENT_Y, MOV_RADIUS)
+//								- Ship.INITIAL_SHIP_PLACEMENT_X;
+//					} else {
+//						pos.y = -MOV_RADIUS - Ship.INITIAL_SHIP_PLACEMENT_Y;
+//						pos.x = 0 + Ship.INITIAL_SHIP_PLACEMENT_X;
+//					}
+//				} else {
+//					Point2d newPos = nearstPointOnCirce(pos, mov, circleSection);
+//					pos.x = newPos.x;
+//					pos.y = newPos.y;
+//				}
+//				break;
+//			case 4:
+//				if (mov.x > -mov.y) {
+//					if (pos.x <= MOV_RADIUS - Ship.INITIAL_SHIP_PLACEMENT_X) {
+//						pos.y = -allowedDistToCircleRadius(pos.x
+//								+ Ship.INITIAL_SHIP_PLACEMENT_X, MOV_RADIUS)
+//								- Ship.INITIAL_SHIP_PLACEMENT_Y;
+//					} else {
+//						pos.x = MOV_RADIUS - Ship.INITIAL_SHIP_PLACEMENT_X;
+//						pos.y = 0 - Ship.INITIAL_SHIP_PLACEMENT_Y;
+//					}
+//				} else if (mov.y < mov.x) {
+//					if (pos.y >= -MOV_RADIUS - Ship.INITIAL_SHIP_PLACEMENT_Y) {
+//						pos.x = allowedDistToCircleRadius(pos.y
+//								+ Ship.INITIAL_SHIP_PLACEMENT_Y, MOV_RADIUS)
+//								- Ship.INITIAL_SHIP_PLACEMENT_X;
+//					} else {
+//						pos.y = -MOV_RADIUS - Ship.INITIAL_SHIP_PLACEMENT_Y;
+//						pos.x = 0 - Ship.INITIAL_SHIP_PLACEMENT_X;
+//					}
+//				} else {
+//					Point2d newPos = nearstPointOnCirce(pos, mov, circleSection);
+//					pos.x = newPos.x;
+//					pos.y = newPos.y;
+//				}
+//
+//			}
+//		}
+//	}
+//
+//	private Point2d[] circleLineIntersection(double r, double xm, double ym,
+//			double m, double n) {
+//		double p = (2 * n * m - 2 * xm - 2 * m * ym - ym) / (1 + m * m) / 2;
+//		double q = (xm * xm + n * n - 2 * n * ym - r * r) / (1 + m * m);
+//		double numResults = p * p - q;
+//		if (numResults < 0) {
+//			return new Point2d[0];
+//		}
+//		double x1 = -p + Math.sqrt(p * p - q);
+//		double y1 = n + m * x1;
+//		if (numResults == 0) {
+//			Point2d[] result = { new Point2d(x1, y1) };
+//			return result;
+//		}
+//		double x2 = -p - Math.sqrt(p * p - q);
+//		double y2 = n + m * x2;
+//		Point2d[] result = { new Point2d(x1, y1), new Point2d(x2, y2) };
+//		return result;
+//	}
+//
+//	private Point2d nearstPointOnCirce(Vector3d pos, Vector3d mov, int section) {
+//		if (mov.x == 0 & mov.y == 0)
+//			return new Point2d(pos.x, pos.y);
+//		double m = mov.y / mov.x;
+//		double n = pos.y - m * pos.x;
+//		Point2d[] intersections = circleLineIntersection(MOV_RADIUS,
+//				-Ship.INITIAL_SHIP_PLACEMENT_X,
+//				-Ship.INITIAL_SHIP_PLACEMENT_Y, m, n);
+//		if (intersections.length < 2) {
+//			return intersections[0];
+//		} else {
+//			double dist1 = Math.sqrt(Math.pow(pos.x - intersections[0].x, 2)
+//					+ Math.pow(pos.y - intersections[0].y, 2));
+//			double dist2 = Math.sqrt(Math.pow(pos.x - intersections[1].x, 2)
+//					+ Math.pow(pos.y - intersections[1].y, 2));
+//			if (dist1 < dist2) {
+//				return intersections[0];
+//			} else {
+//				return intersections[1];
+//			}
+//		}
+//	}
+//
+//	private int outOfCircleSection(Vector3d pos) {
+//		double radius = Math.sqrt(Math.pow(
+//				(pos.x + Ship.INITIAL_SHIP_PLACEMENT_X), 2)
+//				+ Math.pow((pos.y + Ship.INITIAL_SHIP_PLACEMENT_Y), 2));
+//		if (radius <= MOV_RADIUS) {
+//			return 0;
+//		}
+//		if (pos.x >= 0 & pos.y > 0)
+//			return 1;
+//		if (pos.x < 0 & pos.y >= 0)
+//			return 2;
+//		if (pos.x <= 0 & pos.y < 0)
+//			return 3;
+//		return 4;
+//	}
+//
+//	private Point3d shipToViewPosition(double v_x, double v_y) {
+//		Point3d dp = new Point3d();
+//		if (v_x < 0) {
+//			dp.x = -v_x / leftVMax;
+//		} else if (v_x > 0) {
+//			dp.x = v_x / rightVMax;
+//		}
+//		if (v_y < 0) {
+//			dp.y = -v_y / downVMax;
+//		} else if (v_y > 0) {
+//			dp.y = v_y / upVMax;
+//		}
+//		dp.scale(MAX_FOLLOWING_DIST);
+//		if (dp.x != 0 | dp.y != 0) {
+//			System.out.println("dp: " + dp);
+//		}
+//		return dp;
+//	}
 
 	private double allowedDistToCircleRadius(double pos, double radius) {
 		Double dist = Math.sqrt(Math.pow(radius, 2) - Math.pow(pos, 2));
