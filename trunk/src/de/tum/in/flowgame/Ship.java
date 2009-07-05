7eb84de179bca48a76f717c20530f1a82a2527ec
barbara.koehler <barbara.koehler@1012749a-39bf-11de-9d05-31133e6853b1>
2009-07-05 12:14:27 +0000

    ship added in new class edited behaviors added new CollisionBehavior

---
+++ trunk/src/de/tum/in/flowgame/Ship.java
@@ -0,0 +1,116 @@
+package de.tum.in.flowgame;
+
+import java.io.IOException;
+
+import javax.media.j3d.BoundingBox;
+import javax.media.j3d.BranchGroup;
+import javax.media.j3d.Node;
+import javax.media.j3d.Transform3D;
+import javax.media.j3d.TransformGroup;
+import javax.vecmath.Point3d;
+import javax.vecmath.Vector3d;
+
+import de.tum.in.flowgame.behavior.KeyShipBehavior;
+import de.tum.in.flowgame.behavior.ShipCollisionBehavior;
+
+public class Ship extends TransformGroup {
+	
+	public static final float INITIAL_SHIP_PLACEMENT_X = 0;
+	public static final float INITIAL_SHIP_PLACEMENT_Y = -1;
+	public static final float INITIAL_SHIP_PLACEMENT_Z = -6f;
+	private final KeyShipBehavior keyShipBehavior;
+	
+	public Ship(final GameLogic logic, TransformGroup viewTG) throws IOException {
+		super();
+		
+		final Transform3D t3d = new Transform3D();
+		t3d.setTranslation(new Vector3d(INITIAL_SHIP_PLACEMENT_X, INITIAL_SHIP_PLACEMENT_Y, INITIAL_SHIP_PLACEMENT_Z));
+
+		this.setTransform(t3d);
+
+		TransformGroup moveGroup = new TransformGroup();
+		moveGroup.setCapability(TransformGroup.ALLOW_TRANSFORM_WRITE);
+
+		this.addChild(moveGroup);
+
+		TransformGroup rotationGroup = new TransformGroup();
+		rotationGroup.setCapability(TransformGroup.ALLOW_TRANSFORM_WRITE);
+		moveGroup.addChild(rotationGroup);
+
+		TransformGroup ship = loadShip();
+
+		ship.setCollisionBounds(new BoundingBox(new Point3d(-0.35f, -0.15f, -0.5f), new Point3d(0.35f, 0.06f, 0.5f)));
+		// Box box = new Box(0.7f, 0.19f, 1.0f, new Appearance());
+		// box.setCollidable(false);
+		rotationGroup.addChild(ship);
+		System.out.println("Ship " + ship.getBounds());
+
+//		KeyShipEllipseBehavior keyShipBehavior = new KeyShipEllipseBehavior(moveGroup, rotationGroup);
+//		ship.addChild(keyShipBehavior);
+//		keyShipBehavior.setSchedulingBounds(WORLD_BOUNDS);
+		
+		// Alternative KeyBehavior
+		keyShipBehavior = new KeyShipBehavior(moveGroup, viewTG);
+		ship.addChild(keyShipBehavior);
+		keyShipBehavior.setSchedulingBounds(Game3D.WORLD_BOUNDS);
+
+		ShipCollisionBehavior collisionBehavior = new ShipCollisionBehavior(ship, logic);
+		ship.addChild(collisionBehavior);
+		collisionBehavior.setSchedulingBounds(Game3D.WORLD_BOUNDS);
+		
+//		Transform3D staticTransforms = new Transform3D();
+//		
+//		Node node = this;
+//		while(node.getParent() != null){
+//			if(node.getParent() instanceof TransformGroup){
+//				TransformGroup tg = (TransformGroup)node.getParent();
+//				Transform3D trans = new Transform3D();
+//				tg.getTransform(trans);
+//			}
+//		}
+		
+	}
+	
+//	protected static TransformGroup createShip(final GameLogic logic, TransformGroup viewTG) throws IOException {
+//		
+//	}
+
+	private static TransformGroup loadShip() throws IOException {
+
+		final Transform3D rotX = new Transform3D();
+		rotX.rotX(Math.toRadians(90));
+
+		final Transform3D rotY = new Transform3D();
+		rotY.rotY(Math.toRadians(180));
+
+		rotX.mul(rotY);
+
+		TransformGroup rotateShip = new TransformGroup();
+		rotateShip.setTransform(rotX);
+
+		final BranchGroup ship = Utils.loadScene("/res/SFighter.obj");
+
+		rotateShip.addChild(ship);
+
+		return rotateShip;
+	}
+	
+	private Vector3d getShipPos(){
+		
+		//TODO implement
+		return null;
+	}
+
+	public double getXPos() {
+		Vector3d vector = new Vector3d();
+		keyShipBehavior.getVpTrans().get(vector);
+		return vector.getX();
+	}
+
+	public double getYPos() {
+		Vector3d vector = new Vector3d();
+		keyShipBehavior.getVpTrans().get(vector);
+		return vector.getY();
+	}
+
+}
