a7415cf281e5ceca9f35fbfd697590150b05e9d5
google@vxart.de <google@vxart.de@1012749a-39bf-11de-9d05-31133e6853b1>
2009-07-08 16:43:09 +0000

    allow startup without sound device

--- src/de/tum/in/flowgame/util/OALUtil.java
+++ src/de/tum/in/flowgame/util/OALUtil.java
@@ -3,24 +3,43 @@ package de.tum.in.flowgame.util;
 import java.io.InputStream;
 
 import net.java.games.joal.AL;
+import net.java.games.joal.ALException;
 import net.java.games.joal.ALFactory;
 import net.java.games.joal.util.ALut;
 import net.java.games.sound3d.AudioSystem3D;
 import net.java.games.sound3d.Buffer;
 import net.java.games.sound3d.Source;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
 public class OALUtil {
 
+	private static final Log log = LogFactory.getLog(OALUtil.class);
+
 	static {
-		ALut.alutInit();
-		final AL al = ALFactory.getAL();
-		al.alGetError();
-		AudioSystem3D.init();
+		try {
+			ALut.alutInit();
+			final AL al = ALFactory.getAL();
+			final int error = al.alGetError();
+			if (error != AL.AL_NO_ERROR) {
+				throw new ALException("Error " + error);
+			}
+
+			AudioSystem3D.init();
+		} catch (final ALException ex) {
+			log.error("failed to initialize OpenAL" + ex);
+		}
 	}
 
-	public static Source loadSound(final String resource) throws Exception {
-		final InputStream input = OALUtil.class.getResourceAsStream(resource);
-		final Buffer buffer = AudioSystem3D.loadBuffer(input);
-		return AudioSystem3D.generateSource(buffer);
+	public static Source loadSound(final String resource) {
+		try {
+			final InputStream input = OALUtil.class.getResourceAsStream(resource);
+			final Buffer buffer = AudioSystem3D.loadBuffer(input);
+			return AudioSystem3D.generateSource(buffer);
+		} catch (final Exception ex) {
+			log.error("failed to load sound " + resource, ex);
+			return null;
+		}
 	}
 }
