e783641a547a8752f336c666341690abc501f67a
google@vxart.de <google@vxart.de@1012749a-39bf-11de-9d05-31133e6853b1>
2009-07-09 20:21:23 +0000

    DefaultGameListener for classes not interested in all the game state events

---
+++ trunk/src/de/tum/in/flowgame/DefaultGameListener.java
@@ -0,0 +1,35 @@
+package de.tum.in.flowgame;
+
+import de.tum.in.flowgame.GameLogic.Item;
+
+/**
+ * Empty implementations of all {@link GameListener} methods.
+ */
+public abstract class DefaultGameListener implements GameListener {
+
+	@Override
+	public void collided(final GameLogic logic, final Item item) {
+		// empty
+	}
+
+	@Override
+	public void gamePaused(final GameLogic game) {
+		// empty
+	}
+
+	@Override
+	public void gameResumed(final GameLogic game) {
+		// empty
+	}
+
+	@Override
+	public void gameStarted(final GameLogic game) {
+		// TODO empty
+	}
+
+	@Override
+	public void gameStopped(final GameLogic game) {
+		// TODO empty
+	}
+
+}
