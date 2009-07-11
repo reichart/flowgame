8bbc896531f8371a01fdce4d84ec154b57621f09
huebnerc <huebnerc@1012749a-39bf-11de-9d05-31133e6853b1>
2009-07-11 23:10:15 +0000

    improved collision detection algorithm (added linear interpolation of ship position)

--- src/de/tum/in/flowgame/behavior/CollisionBehavior.java
+++ src/de/tum/in/flowgame/behavior/CollisionBehavior.java
@@ -20,8 +20,13 @@ public class CollisionBehavior extends Behavior {
 	private final GameLogic gameLogic;
 	private final BranchGroup branchGroup;
 	private final Ship ship;
+	private double shipX = Ship.INITIAL_SHIP_PLACEMENT_X;
+	private double shipY = Ship.INITIAL_SHIP_PLACEMENT_Y;
+	private double shipOldX;
+	private double shipOldY;
 
-	public CollisionBehavior(final BranchGroup collidables, final GameLogic gl, final Ship ship) {
+	public CollisionBehavior(final BranchGroup collidables, final GameLogic gl,
+			final Ship ship) {
 		condition = new WakeupOnElapsedFrames(0);
 		branchGroup = collidables;
 		gameLogic = gl;
@@ -37,11 +42,13 @@ public class CollisionBehavior extends Behavior {
 	@Override
 	public void processStimulus(final Enumeration criteria) {
 		final Enumeration<Node> children = branchGroup.getAllChildren();
-		
+
+		shipOldX = shipX;
+		shipOldY = shipY;
 		final Vector3d shipCoords = ship.getControls().getCoords();
-		final double shipX = shipCoords.getX();
-		final double shipY = shipCoords.getY();
-//		final double shipZ = shipCoords.getZ();
+		shipX = shipCoords.getX();
+		shipY = shipCoords.getY();
+		// final double shipZ = shipCoords.getZ();
 
 		while (children.hasMoreElements()) {
 			final Node child = children.nextElement();
@@ -49,28 +56,35 @@ public class CollisionBehavior extends Behavior {
 				final Collidable collidable = (Collidable) child;
 
 				final double zPos = collidable.getZPos();
-//				final double xPos = collidable.getXPos();
-//				final double yPos = collidable.getYPos();
+				// final double xPos = collidable.getXPos();
+				// final double yPos = collidable.getYPos();
 
-				final double oldz = collidable.getOldZPos();
+				final double oldZ = collidable.getOldZPos();
 
 				if (zPos > Ship.INITIAL_SHIP_PLACEMENT_Z
-						&& oldz <= Ship.INITIAL_SHIP_PLACEMENT_Z) {
+						&& oldZ <= Ship.INITIAL_SHIP_PLACEMENT_Z) {
 
 					final Bounds collidableBounds = collidable.getBounds();
-					final BoundingBox shipBounds = new BoundingBox();
-					shipBounds.setLower(shipX - 0.6, shipY - 0.15, oldz);
-					shipBounds.setUpper(shipX + 0.6, shipY + 0.15, zPos);
-//					ship.setBounds(shipBounds);
 
+					double factor = (zPos - Ship.INITIAL_SHIP_PLACEMENT_Z)/(zPos-oldZ);
+					double xDist = shipX - shipOldX;
+					double colX = shipOldX + xDist * factor;
+					double yDist = shipY - shipOldY;
+					double colY = shipOldY + yDist * factor;
+					final BoundingBox shipBounds = new BoundingBox();
+					shipBounds.setLower(colX - 0.6, colY - 0.15, oldZ);
+					shipBounds.setUpper(colX + 0.6, colY + 0.15, zPos);
+					// ship.setBounds(shipBounds);
 
 					if (collidableBounds.intersect(shipBounds)) {
-//						System.out.println("COLLISION! BAAAAM!" + oldz + ", "
-//								+ zPos + child.getUserData());
-//						System.out.println("shipX: " + shipX + " - shipY: " + shipY
-//								+ " - shipZ: " + shipZ);
-//						System.out.println("colX: " + xPos + " - colY: " + yPos
-//								+ " - colZ: " + zPos);
+						// System.out.println("COLLISION! BAAAAM!" + oldz + ", "
+						// + zPos + child.getUserData());
+						// System.out.println("shipX: " + shipX + " - shipY: " +
+						// shipY
+						// + " - shipZ: " + shipZ);
+						// System.out.println("colX: " + xPos + " - colY: " +
+						// yPos
+						// + " - colZ: " + zPos);
 						gameLogic.collide(child);
 					}
 					((BranchGroup) child).detach();
