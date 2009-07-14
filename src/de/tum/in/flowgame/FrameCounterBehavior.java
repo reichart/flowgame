888933219e88ccd7344f7eef8c45d58fb33a5ae0
google@vxart.de <google@vxart.de@1012749a-39bf-11de-9d05-31133e6853b1>
2009-07-14 21:53:00 +0000

    fixed FPS calculation

--- src/de/tum/in/flowgame/FrameCounterBehavior.java
+++ src/de/tum/in/flowgame/FrameCounterBehavior.java
@@ -6,22 +6,30 @@ import javax.media.j3d.Behavior;
 import javax.media.j3d.WakeupCondition;
 import javax.media.j3d.WakeupOnElapsedFrames;
 
+/**
+ * Measures frame rate over a number of frames and delivers them to subscribed
+ * listeners.
+ */
 public class FrameCounterBehavior extends Behavior {
 
 	public interface FrameCounterListener {
 		void updateFramesPerSecond(long fps);
 	}
-	
+
 	private final Listeners<FrameCounterListener> listeners;
-	
 	private final WakeupCondition condition;
+	private final long frames;
 
-	private long start = 0;
+	private long start;
+	private long fps;
 
-	private long timeTaken;
-
-	public FrameCounterBehavior(final int interval) {
-		this.condition = new WakeupOnElapsedFrames(interval);
+	/**
+	 * @param frames
+	 *            number of frames to average frame rate over
+	 */
+	public FrameCounterBehavior(final int frames) {
+		this.frames = frames;
+		this.condition = new WakeupOnElapsedFrames(frames);
 		this.listeners = new Listeners<FrameCounterListener>() {
 			@Override
 			protected void fire(final Object event, final FrameCounterListener listener) {
@@ -33,9 +41,10 @@ public class FrameCounterBehavior extends Behavior {
 	public Listeners<FrameCounterListener> getListeners() {
 		return listeners;
 	}
-	
+
 	@Override
 	public void initialize() {
+		this.start = System.currentTimeMillis();
 		wakeupOn(condition);
 	}
 
@@ -43,15 +52,15 @@ public class FrameCounterBehavior extends Behavior {
 	@Override
 	public void processStimulus(final Enumeration/* <WakeupCriterion> */criteria) {
 		final long now = System.currentTimeMillis();
-		this.timeTaken = now - start;
+		this.fps = (frames * 1000) / (now - start);
 		this.start = now;
-		
+
 		listeners.fire(null);
-		
+
 		wakeupOn(condition);
 	}
 
 	public long getFramesPerSecond() {
-		return timeTaken * 1000 / timeTaken;
+		return fps;
 	}
 }
