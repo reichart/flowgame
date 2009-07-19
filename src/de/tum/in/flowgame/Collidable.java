3995c288f1f060d2aed7f682c9b5ab3fb6edb51f
huebnerc <huebnerc@1012749a-39bf-11de-9d05-31133e6853b1>
2009-07-19 21:24:33 +0000

    Now real equipartition of collidables on circle area.

--- src/de/tum/in/flowgame/Collidable.java
+++ src/de/tum/in/flowgame/Collidable.java
@@ -17,8 +17,8 @@ public class Collidable extends BranchGroup {
 	private final double yPos;
 	final private ForwardNavigatorBehavior fwdNav;
 
-	public Collidable(final SharedGroup group, long speed,
-			float scale, GameLogic gameLogic) {
+	public Collidable(final SharedGroup group, long speed, float scale,
+			GameLogic gameLogic) {
 		this.setCapability(BranchGroup.ALLOW_DETACH);
 
 		setUserData(group.getUserData());
@@ -27,61 +27,68 @@ public class Collidable extends BranchGroup {
 		link.setBoundsAutoCompute(true);
 
 		long duration = (long) (3000 + (Math.random() * 6000));
-//		System.out.println(duration);
+		// System.out.println(duration);
 		Alpha rotAlpha = new Alpha(-1, duration);
 		rotAlpha.setStartTime(System.currentTimeMillis());
 
-		final TransformGroup shape = new TransformGroupBuilder()
-				.add(link)
+		final TransformGroup shape = new TransformGroupBuilder().add(link)
 				.addRotationBehavior(rotAlpha, Game3D.WORLD_BOUNDS)
-				.computeAutoBounds(true)
-				.fin();
+				.computeAutoBounds(true).fin();
 
-		final TransformGroup scaledShape = new TransformGroupBuilder()
-				.scale(scale)
-				.add(shape)
-				.computeAutoBounds(true)
-				.fin();
+		final TransformGroup scaledShape = new TransformGroupBuilder().scale(
+				scale).add(shape).computeAutoBounds(true).fin();
 
 		final int parts = Math.max(1, Tunnel.TUNNEL_PARTS - 1);
 		final float endOfTunnel = -parts * Tunnel.TUNNEL_LENGTH;
-		
-		final TransformGroup tg = new TransformGroupBuilder()
-				.translate(0, 0, endOfTunnel)
-				.add(scaledShape)
-				.writable()
-				.computeAutoBounds(true)
-				.fin();
+
+		final TransformGroup tg = new TransformGroupBuilder().translate(0, 0,
+				endOfTunnel).add(scaledShape).writable()
+				.computeAutoBounds(true).fin();
 
 		fwdNav = new ForwardNavigatorBehavior(tg, speed);
 		fwdNav.setSchedulingBounds(Game3D.WORLD_BOUNDS);
 		gameLogic.addListener(fwdNav);
 		tg.addChild(fwdNav);
 
-		final TransformGroup scaleTG = new TransformGroupBuilder()
-				.add(tg)
-				.computeAutoBounds(true)
-				.fin();
+		final TransformGroup scaleTG = new TransformGroupBuilder().add(tg)
+				.computeAutoBounds(true).fin();
 
 		scaleTG.setBoundsAutoCompute(true);
+		
+		Point2d position = new Point2d(Math.random()
+				* KeyShipBehavior.MOV_RADIUS * 2 - KeyShipBehavior.MOV_RADIUS,
+				Math.random() * KeyShipBehavior.MOV_RADIUS * 2
+						- KeyShipBehavior.MOV_RADIUS);
+		;
+		while (!withinCircle(position)) {
+			position = new Point2d(Math.random() * KeyShipBehavior.MOV_RADIUS,
+					Math.random() * KeyShipBehavior.MOV_RADIUS);
+		}
 
-		Point2d position = polarToCartesian(Math.random()*KeyShipBehavior.MOV_RADIUS, Math.random() * 2 * Math.PI);
 		xPos = position.getX();
 		yPos = position.getY();
 
-//		 xPos = Ship.INITIAL_SHIP_PLACEMENT_X;
-//		 yPos = Ship.INITIAL_SHIP_PLACEMENT_Y;
+		// xPos = Ship.INITIAL_SHIP_PLACEMENT_X;
+		// yPos = Ship.INITIAL_SHIP_PLACEMENT_Y;
 
-		TransformGroup transTG = new TransformGroupBuilder()
-			.translate(xPos,yPos, 0).add(scaleTG)
-			.fin();
+		TransformGroup transTG = new TransformGroupBuilder().translate(xPos,
+				yPos, 0).add(scaleTG).fin();
 		addChild(transTG);
 	}
-	
-	private Point2d polarToCartesian (double radius, double angleInRadians){
-		double x = Math.cos( angleInRadians ) * radius;
-		double y = Math.sin( angleInRadians ) * radius;
-		return new Point2d(x,y);
+
+	private Point2d polarToCartesian(double radius, double angleInRadians) {
+		double x = Math.cos(angleInRadians) * radius;
+		double y = Math.sin(angleInRadians) * radius;
+		return new Point2d(x, y);
+	}
+
+	private boolean withinCircle(Point2d position) {
+		double radius = Math.sqrt(Math.pow(position.getX(), 2)
+				+ Math.pow(position.getY(), 2));
+		if (radius <= KeyShipBehavior.MOV_RADIUS)
+			return true;
+		else
+			return false;
 	}
 
 	public double getOldZPos() {
