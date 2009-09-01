db19482a27d12319d3a6d81c9d69e86514ae0cb3
barbara.koehler <barbara.koehler@1012749a-39bf-11de-9d05-31133e6853b1>
2009-09-01 08:36:19 +0000

    made serializable

--- trunk/src/de/tum/in/flowgame/model/DifficultyFunction.java
+++ trunk/src/de/tum/in/flowgame/model/DifficultyFunction.java
@@ -1,9 +1,11 @@
 package de.tum.in.flowgame.model;
 
+import java.io.Serializable;
+
 import javax.persistence.Entity;
 
 @Entity
-public class DifficultyFunction extends AbstractEntity {
+public class DifficultyFunction extends AbstractEntity implements Serializable {
 	Function intervald;
 	Function speed;
 	Function ratio;
