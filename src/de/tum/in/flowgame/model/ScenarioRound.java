f1f7c0d2c1b9274277c79fdc2cd9fac5a282e2f2
barbara.koehler <barbara.koehler@1012749a-39bf-11de-9d05-31133e6853b1>
2009-09-06 16:06:04 +0000

    added new variable for counting progress

--- src/de/tum/in/flowgame/model/ScenarioRound.java
+++ src/de/tum/in/flowgame/model/ScenarioRound.java
@@ -10,32 +10,37 @@ public class ScenarioRound extends AbstractEntity implements Serializable {
 	Long expectedPlaytime;
 	DifficultyFunction difficutyFunction;
 	Questionnaire questionnaire;
-	
-	
+
 	public Integer getBaselineModifier() {
 		return baselineModifier;
 	}
+
 	public void setBaselineModifier(Integer baselineModifier) {
 		this.baselineModifier = baselineModifier;
 	}
+
 	public Long getExpectedPlaytime() {
 		return expectedPlaytime;
 	}
+
 	public void setExpectedPlaytime(Long expectedPlaytime) {
 		this.expectedPlaytime = expectedPlaytime;
 	}
+
 	public DifficultyFunction getDifficutyFunction() {
 		return difficutyFunction;
 	}
+
 	public void setDifficutyFunction(DifficultyFunction difficutyFunction) {
 		this.difficutyFunction = difficutyFunction;
 	}
+
 	public Questionnaire getQuestionnaire() {
 		return questionnaire;
 	}
+
 	public void setQuestionnaire(Questionnaire questionnaire) {
 		this.questionnaire = questionnaire;
 	}
-	
-	
+
 }
