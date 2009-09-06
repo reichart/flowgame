152862736f54d4ef9976ecad6464b3d06f1f82ed
huebnerc <huebnerc@1012749a-39bf-11de-9d05-31133e6853b1>
2009-09-06 21:41:36 +0000

    Added sliders for max_speed and acceleration to settings menu

--- trunk/src/de/tum/in/flowgame/ui/screens/SettingsScreen.java
+++ trunk/src/de/tum/in/flowgame/ui/screens/SettingsScreen.java
@@ -2,8 +2,10 @@ package de.tum.in.flowgame.ui.screens;
 
 import java.awt.Container;
 import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.AbstractAction;
+import javax.swing.BorderFactory;
 import javax.swing.Box;
 import javax.swing.BoxLayout;
 import javax.swing.ButtonGroup;
@@ -12,6 +14,9 @@ import javax.swing.JButton;
 import javax.swing.JCheckBox;
 import javax.swing.JLabel;
 import javax.swing.JRadioButton;
+import javax.swing.JSlider;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 
 import de.tum.in.flowgame.GameLogic;
 import de.tum.in.flowgame.Ship;
@@ -26,7 +31,6 @@ public class SettingsScreen extends MenuScreen {
 	private final JRadioButton normal = new JRadioButton(new AbstractAction("normal") {
 		@Override
 		public void actionPerformed(final ActionEvent e) {
-			System.out.println("normal");
 			menu.getGame().getShip().getControls().setNormalSteering(true);
 		}
 	});
@@ -34,7 +38,6 @@ public class SettingsScreen extends MenuScreen {
 	private final JRadioButton airplane = new JRadioButton(new AbstractAction("airplane") {
 		@Override
 		public void actionPerformed(final ActionEvent e) {
-			System.out.println("airplane");
 			menu.getGame().getShip().getControls().setNormalSteering(false);
 		}
 	});
@@ -46,17 +49,39 @@ public class SettingsScreen extends MenuScreen {
 		}
 	});
 	
+	private final JSlider accSlider = new JSlider(500, 10000);	
+	private final JSlider maxSpeedSlider = new JSlider(500, 10000);
+	
 	private final JButton back = new JButton(new AbstractAction("Back") {
 		@Override
 		public void actionPerformed(final ActionEvent e) {
 			menu.show(MainScreen.class);
 		}
 	});
+	
 
 	public SettingsScreen(final GameMenu menu) {
 		super(menu);
-		menu.getGame().getShip().getControls().setNormalSteering(true);
-		normal.setSelected(true);
+		accSlider.addChangeListener(new ChangeListener() {
+			@Override
+			public void stateChanged(ChangeEvent e) {
+//				System.out.println("Acceleration: " + (float)accSlider.getValue()/100);
+				menu.getGame().getShip().getControls().setAcceleration((float)accSlider.getValue()/100);
+			}
+		});
+		maxSpeedSlider.addChangeListener(new ChangeListener() {
+			@Override
+			public void stateChanged(ChangeEvent e) {
+//				System.out.println("Max Speed: " + (float)maxSpeedSlider.getValue()/100);
+				menu.getGame().getShip().getControls().setMaxSpeed((float)maxSpeedSlider.getValue()/100);
+			}
+		});
+		accSlider.setValue((int) menu.getGame().getShip().getControls().getAcceleration()*100);
+		accSlider.setBorder(BorderFactory.createTitledBorder("Acceleration"));
+		maxSpeedSlider.setValue((int) menu.getGame().getShip().getControls().getMax_speed()*100);
+		maxSpeedSlider.setBorder(BorderFactory.createTitledBorder("Max Speed"));
+		normal.setSelected(menu.getGame().getShip().getControls().getNormalSteering());
+		airplane.setSelected(!menu.getGame().getShip().getControls().getNormalSteering());
 		steeringgroup.add(normal);
 		steeringgroup.add(airplane);
 		steeringbox.add(steering);
@@ -66,7 +91,7 @@ public class SettingsScreen extends MenuScreen {
 
 	@Override
 	public Container getContents() {
-		return centered(title("Settings"), steeringbox, fps, back);
+		return centered(title("Settings"), steeringbox, fps, accSlider, maxSpeedSlider, back);
 	}
 
 }
