567a7d14ff6a0f2a20f45452ea520066300e592e
barbara.koehler <barbara.koehler@1012749a-39bf-11de-9d05-31133e6853b1>
2009-09-06 16:06:04 +0000

    added new variable for counting progress

--- trunk/src/de/tum/in/flowgame/model/ScenarioSession.java
+++ trunk/src/de/tum/in/flowgame/model/ScenarioSession.java
@@ -8,6 +8,7 @@ import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.OneToMany;
+import javax.persistence.Transient;
 
 @Entity
 public class ScenarioSession extends AbstractEntity implements Serializable{
@@ -15,12 +16,26 @@ public class ScenarioSession extends AbstractEntity implements Serializable{
 	List<ScenarioRound> rounds;
 	String name;
 	
+	@Transient
+	int roundsPlayed;
+	
 	public ScenarioSession() {
 		rounds = new ArrayList<ScenarioRound>();
+		roundsPlayed = 0;
 	}
 
 	public List<ScenarioRound> getRounds() {
 		return rounds;
+	}
+	
+	// returns next round and counts how many rounds have been played
+	public ScenarioRound getNextRound() {
+		ScenarioRound round = null;
+		if (rounds.size() > roundsPlayed) {
+			round = rounds.get(roundsPlayed);
+		}
+		roundsPlayed++;
+		return round;
 	}
 	
 }
