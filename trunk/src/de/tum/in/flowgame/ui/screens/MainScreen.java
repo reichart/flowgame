884339dc0978632594bae033d223c03dddce3286
google@vxart.de <google@vxart.de@1012749a-39bf-11de-9d05-31133e6853b1>
2009-09-06 17:40:59 +0000

    clean up

--- trunk/src/de/tum/in/flowgame/ui/screens/MainScreen.java
+++ trunk/src/de/tum/in/flowgame/ui/screens/MainScreen.java
@@ -10,26 +10,23 @@ import de.tum.in.flowgame.ui.GameMenu;
 
 public class MainScreen extends MenuScreen {
 
-	final JButton play = new JButton(new AbstractAction("Play") {
+	private final JButton play = new JButton(new AbstractAction("Play") {
 		@Override
 		public void actionPerformed(final ActionEvent e) {
-			System.out.println("MainMenu.play.new AbstractAction() {...}.actionPerformed()");
 			menu.getLogic().start();
 		}
 	});
 
-	final JButton highscores = new JButton(new AbstractAction("Highscores") {
+	private final JButton highscores = new JButton(new AbstractAction("Highscores") {
 		@Override
 		public void actionPerformed(final ActionEvent e) {
-			System.out.println("MainMenu.highscores.new AbstractAction() {...}.actionPerformed()");
 			menu.show(HighscoresScreen.class);
 		}
 	});
 
-	final JButton settings = new JButton(new AbstractAction("Settings") {
+	private final JButton settings = new JButton(new AbstractAction("Settings") {
 		@Override
-		public void actionPerformed(ActionEvent e) {
-			System.out.println("MainMenu.settings.new AbstractAction() {...}.actionPerformed()");
+		public void actionPerformed(final ActionEvent e) {
 			menu.show(SettingsScreen.class);
 		}
 	});
