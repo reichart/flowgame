4e265dfd60363c5bc225d18d7ea9a3e53cadaa76
google@vxart.de <google@vxart.de@1012749a-39bf-11de-9d05-31133e6853b1>
2009-09-06 13:47:38 +0000

    use undecorated frame for performance reasons

--- trunk/src/de/tum/in/flowgame/GameApplet.java
+++ trunk/src/de/tum/in/flowgame/GameApplet.java
@@ -2,7 +2,9 @@ package de.tum.in.flowgame;
 
 import java.applet.Applet;
 import java.awt.BorderLayout;
+import java.awt.Dimension;
 import java.awt.Frame;
+import java.awt.Toolkit;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
@@ -13,15 +15,23 @@ public class GameApplet extends Applet {
 	private final Game3D game;
 
 	public static void main(final String[] args) throws Exception {
+		final Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
 		final Frame frame = new Frame();
-		frame.setSize(800, 600);
-//		frame.setUndecorated(true);
+		frame.setUndecorated(true);
+		frame.setResizable(false);
+		frame.setSize(600, 400);
+		
+		final int centerX = (screen.width - frame.getWidth()) / 2;
+		final int centerY = (screen.height - frame.getHeight()) / 2;
+		frame.setLocation(centerX , centerY);
+		
 		frame.addWindowListener(new WindowAdapter() {
 			@Override
 			public void windowClosing(final WindowEvent e) {
 				System.exit(0);
 			}
 		});
+		
 		final GameApplet app = new GameApplet();
 		frame.add(app);
 		frame.setVisible(true);
