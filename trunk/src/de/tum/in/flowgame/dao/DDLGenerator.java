572c28e0d52b1a5f629515eed075a7af152b5eef
google@vxart.de <google@vxart.de@1012749a-39bf-11de-9d05-31133e6853b1>
2009-10-05 20:30:02 +0000

    we use UTF-8, not CP1252

--- trunk/src/de/tum/in/flowgame/dao/DDLGenerator.java
+++ trunk/src/de/tum/in/flowgame/dao/DDLGenerator.java
@@ -31,35 +31,35 @@ public class DDLGenerator {
 
 			
 			Question q1 = new Question();
-			q1.setText("Viel Leute halten mich für etwas kühl und distanziert.");
+			q1.setText("Viel Leute halten mich fÃ¼r etwas kÃ¼hl und distanziert.");
 			Question q2 = new Question();
-			q2.setText("Probleme, die schwierig zu lösen sind, reizen mich.");
+			q2.setText("Probleme, die schwierig zu lÃ¶sen sind, reizen mich.");
 			Question q3 = new Question();
-			q3.setText("Ich bin ein fröhlicher, gut gelaunter Mensch.");
+			q3.setText("Ich bin ein frÃ¶hlicher, gut gelaunter Mensch.");
 			Question q4 = new Question();
-			q4.setText("Zu häufig bin ich entmutigt und will aufgeben, wenn etwas schief geht.");
+			q4.setText("Zu hÃ¤ufig bin ich entmutigt und will aufgeben, wenn etwas schief geht.");
 			Question q5 = new Question();
-			q5.setText("Ich strebe danach, alles mir Mögliche zu erreichen.");
+			q5.setText("Ich strebe danach, alles mir MÃ¶gliche zu erreichen.");
 			Question q6 = new Question();
-			q6.setText("Ich bin häufig beunruhigt, über Dinge, die schief gehen könnten.");
+			q6.setText("Ich bin hÃ¤ufig beunruhigt, Ã¼ber Dinge, die schief gehen kÃ¶nnten.");
 			Question q7 = new Question();
-			q7.setText("Ich fühle mich besonders erfolgreich, wenn ich eine neue Idee darüber bekommen habe, wie eine Sache funktioniert.");
+			q7.setText("Ich fÃ¼hle mich besonders erfolgreich, wenn ich eine neue Idee darÃ¼ber bekommen habe, wie eine Sache funktioniert.");
 			Question q8 = new Question();
 			q8.setText("Ich arbeite zielstrebig und effektiv.");
 			Question q9 = new Question();
-			q9.setText("Ich ziehe es gewöhnlich vor, Dinge allein zu tun.");
+			q9.setText("Ich ziehe es gewÃ¶hnlich vor, Dinge allein zu tun.");
 			Question q10 = new Question();
-			q10.setText("Mich reizen Situationen, in denen ich meine Fähigkeiten testen kann.");
+			q10.setText("Mich reizen Situationen, in denen ich meine FÃ¤higkeiten testen kann.");
 			Question q11 = new Question();
-			q11.setText("Ich fühle mich oft hilflos und wünsche mir eine Person, die meine Probleme löst.");
+			q11.setText("Ich fÃ¼hle mich oft hilflos und wÃ¼nsche mir eine Person, die meine Probleme lÃ¶st.");
 			Question q12 = new Question();
 			q12.setText("Ich habe gerne viele Leute um mich herum.");
 			Question q13 = new Question();
-			q13.setText("Ich fühle mich besonders erfolgreich, wenn ich eine wirklich komplizierte Sache endgültig verstanden habe.");
+			q13.setText("Ich fÃ¼hle mich besonders erfolgreich, wenn ich eine wirklich komplizierte Sache endgÃ¼ltig verstanden habe.");
 			Questionnaire qn = new Questionnaire();
-			qn.setName("Persönlichkeitsbeschreibung");
-			qn.setDescription("Die Folgenden eignen sich zur Beschreibung Ihrer eigenen Person (insgesamt 13 Aussagen). Lesen Sie bitte jede Aussage aufmerksam durch. Zur Beantwortung steht Ihnen eine kontinuierlich Skala von starker Ablehnung (d.h. die Beschreibung trifft überhaupt nicht auf Sie zu) bis zu einer starken Zustimmung (d.h. die Beschreibung trifft voll auf Sie zu) zur Verfügung." +
-				"Es gibt keine „richtigen“ oder „falschen“ Antworten. Sie bringen mit Ihren Antworten vielmehr Ihre persönliche Sichtweise zum Ausdruck. Wenn Ihnen einmal die Entscheidung schwer fallen sollte, geben Sie dann die Ausprägung an, die noch am ehesten auf Sie zutrifft.");
+			qn.setName("PersÃ¶nlichkeitsbeschreibung");
+			qn.setDescription("Die Folgenden eignen sich zur Beschreibung Ihrer eigenen Person (insgesamt 13 Aussagen). Lesen Sie bitte jede Aussage aufmerksam durch. Zur Beantwortung steht Ihnen eine kontinuierlich Skala von starker Ablehnung (d.h. die Beschreibung trifft Ã¼berhaupt nicht auf Sie zu) bis zu einer starken Zustimmung (d.h. die Beschreibung trifft voll auf Sie zu) zur VerfÃ¼gung." +
+				"Es gibt keine â€žrichtigenâ€œ oder â€žfalschenâ€œ Antworten. Sie bringen mit Ihren Antworten vielmehr Ihre persÃ¶nliche Sichtweise zum Ausdruck. Wenn Ihnen einmal die Entscheidung schwer fallen sollte, geben Sie dann die AusprÃ¤gung an, die noch am ehesten auf Sie zutrifft.");
 			qn.addQuestion(q1);
 			qn.addQuestion(q2);
 			qn.addQuestion(q3);
