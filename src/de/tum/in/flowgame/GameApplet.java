027587a22b0068d6a5f9e97b81d2c5adc9d3993e
google@vxart.de <google@vxart.de@1012749a-39bf-11de-9d05-31133e6853b1>
2009-10-15 13:58:05 +0000

    get user ID and name from Facebook

--- src/de/tum/in/flowgame/GameApplet.java
+++ src/de/tum/in/flowgame/GameApplet.java
@@ -7,16 +7,21 @@ import java.awt.Frame;
 import java.awt.Toolkit;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.net.MalformedURLException;
+import java.net.URL;
 
-import javax.swing.JOptionPane;
+import com.google.code.facebookapi.ProfileField;
 
 import de.tum.in.flowgame.client.Client;
 import de.tum.in.flowgame.model.Person;
+import de.tum.in.flowgame.util.CustomFacebookClient;
 
 public class GameApplet extends Applet {
 
 	private final Game3D game;
 
+	private final CustomFacebookClient facebook;
+	
 	public static void main(final String[] args) throws Exception {
 		final Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
 		final Frame frame = new Frame();
@@ -43,17 +48,26 @@ public class GameApplet extends Applet {
 	}
 
 	public GameApplet() throws Exception {
-		//TODO: get id from facebook
-		final String sid = JOptionPane.showInputDialog("Bitte id eingeben");
-		final Long id = Long.decode(sid);
+		final String server = getParameter("server");
+		final String apiKey = getParameter("apiKey");
+		final String sessionKey = getParameter("sessionKey");
+		final String sessionSecret = getParameter("sessionSecret");
 		
-		final Client client = new Client("INSERT SERVER HERE");
+		try {
+			this.facebook = new CustomFacebookClient(new URL(server + "fbproxy"), apiKey, sessionSecret, sessionKey);
+		} catch (final MalformedURLException ex) {
+			throw new RuntimeException("Failed to initialize Facebook client", ex);
+		}
+		
+		final long id = facebook.users_getLoggedInUser();
+		
+		Client client = new Client(server);
 		
 		// download person information from server
 		Person player = client.downloadPerson(id);
 		if (player == null) {
 			player = new Person(id);
-			String name = JOptionPane.showInputDialog("Bitte Name eingeben");
+			final String name = facebook.users_getProfileField(id, ProfileField.FIRST_NAME);
 			player.setName(name);
 			client.uploadQuietly(player);
 		}
@@ -63,5 +77,6 @@ public class GameApplet extends Applet {
 		setLayout(new BorderLayout());
 		add(BorderLayout.CENTER, game);
 	}
+
 
 }
---
+++ src/de/tum/in/flowgame/util/CustomFacebookClient.java
@@ -0,0 +1,35 @@
+package de.tum.in.flowgame.util;
+
+import java.net.URL;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.google.code.facebookapi.FacebookException;
+import com.google.code.facebookapi.FacebookJsonRestClient;
+import com.google.code.facebookapi.ProfileField;
+
+public class CustomFacebookClient extends FacebookJsonRestClient {
+
+	public CustomFacebookClient(final URL serverUrl, final String apiKey, final String secret, final String sessionKey) {
+		super(serverUrl, apiKey, secret, sessionKey);
+	}
+
+	public JSONObject users_getInfo(final long userId, final ProfileField... fields) throws FacebookException,
+			JSONException {
+		final Set<Long> userIds = Collections.singleton(userId);
+		final List<ProfileField> fieldList = Arrays.asList(fields);
+		final JSONArray response = (JSONArray) users_getInfo(userIds, fieldList);
+		return response.getJSONObject(0);
+	}
+
+	public String users_getProfileField(final long userId, final ProfileField field) throws FacebookException,
+			JSONException {
+		return users_getInfo(userId, field).getString(field.fieldName());
+	}
+}
