cd297c347f4eef441d4b69896514f44c51490fa4
google@vxart.de <google@vxart.de@1012749a-39bf-11de-9d05-31133e6853b1>
2009-10-15 23:06:02 +0000

    cleanup

--- trunk/src/de/tum/in/flowgame/Ship.java
+++ trunk/src/de/tum/in/flowgame/Ship.java
@@ -9,6 +9,7 @@ import java.util.TimerTask;
 import javax.media.j3d.Appearance;
 import javax.media.j3d.BoundingBox;
 import javax.media.j3d.BranchGroup;
+import javax.media.j3d.Group;
 import javax.media.j3d.Material;
 import javax.media.j3d.Node;
 import javax.media.j3d.Shape3D;
@@ -16,8 +17,6 @@ import javax.media.j3d.Texture;
 import javax.media.j3d.Transform3D;
 import javax.media.j3d.TransformGroup;
 import javax.vecmath.Color3f;
-import javax.vecmath.Matrix3d;
-import javax.vecmath.Matrix3f;
 import javax.vecmath.Point3d;
 import javax.vecmath.Vector3d;
 
@@ -31,59 +30,43 @@ public class Ship extends TransformGroup implements GameListener {
 	public static final float INITIAL_SHIP_PLACEMENT_Y = -1;
 	public static final float INITIAL_SHIP_PLACEMENT_Z = -6f;
 	private final ShipNavigationBehavior shipNavigationBehavior;
-	
+
 	private final Transform3D staticTransforms;
 
 	private Shape3D shape1;
-//	private Texture tex1;
 	private Shape3D shape2;
 	private Texture tex2;
 
-	private Timer flashTimer;
-	private SpeedChangeBehavior speedChange;
+	private final Timer flashTimer;
+	private final SpeedChangeBehavior speedChange;
 
-	public Ship(final GameLogic gameLogic, TransformGroup viewTG)
-			throws IOException {
-		super();
-
+	public Ship(final GameLogic gameLogic, final TransformGroup viewTG) throws IOException {
 		this.flashTimer = new Timer("FlashTimer", true);
 		this.setBoundsAutoCompute(false);
 
 		final Transform3D t3d = new Transform3D();
-		t3d.setTranslation(new Vector3d(INITIAL_SHIP_PLACEMENT_X,
-				INITIAL_SHIP_PLACEMENT_Y, INITIAL_SHIP_PLACEMENT_Z));
+		t3d.setTranslation(new Vector3d(INITIAL_SHIP_PLACEMENT_X, INITIAL_SHIP_PLACEMENT_Y, INITIAL_SHIP_PLACEMENT_Z));
 
 		this.setTransform(t3d);
 
-		TransformGroup moveGroup = new TransformGroup();
+		final TransformGroup moveGroup = new TransformGroup();
 		moveGroup.setCapability(TransformGroup.ALLOW_TRANSFORM_WRITE);
 
 		this.addChild(moveGroup);
 
-		TransformGroup rotationGroup = new TransformGroup();
+		final TransformGroup rotationGroup = new TransformGroup();
 		rotationGroup.setCapability(TransformGroup.ALLOW_TRANSFORM_WRITE);
 		moveGroup.addChild(rotationGroup);
 
-		TransformGroup ship = loadShip();
+		final TransformGroup ship = loadShip();
 
-//		ship.setCollisionBounds(new BoundingBox(new Point3d(-0.35f, -0.15f,
-//				-0.5f), new Point3d(0.35f, 0.06f, 0.5f)));
-		// Box box = new Box(0.7f, 0.19f, 1.0f, new Appearance());
-		// box.setCollidable(false);
 		rotationGroup.addChild(ship);
-//		System.out.println("Ship " + ship.getBounds());
 
-		// KeyShipEllipseBehavior shipNavigationBehavior = new
-		// KeyShipEllipseBehavior(moveGroup, rotationGroup);
-		// ship.addChild(shipNavigationBehavior);
-		// shipNavigationBehavior.setSchedulingBounds(WORLD_BOUNDS);
-
-		// Alternative KeyBehavior
 		shipNavigationBehavior = new ShipNavigationBehavior(moveGroup, viewTG, gameLogic);
 		gameLogic.addListener(shipNavigationBehavior);
 		ship.addChild(shipNavigationBehavior);
 		shipNavigationBehavior.setSchedulingBounds(Game3D.WORLD_BOUNDS);
-		
+
 		speedChange = new SpeedChangeBehavior(shipNavigationBehavior, gameLogic);
 		speedChange.setSchedulingBounds(Game3D.WORLD_BOUNDS);
 		addChild(speedChange);
@@ -93,8 +76,8 @@ public class Ship extends TransformGroup implements GameListener {
 		Node node = this;
 		while (node.getParent() != null) {
 			if (node.getParent() instanceof TransformGroup) {
-				TransformGroup tg = (TransformGroup) node.getParent();
-				Transform3D trans = new Transform3D();
+				final TransformGroup tg = (TransformGroup) node.getParent();
+				final Transform3D trans = new Transform3D();
 				tg.getTransform(trans);
 				staticTransforms.mul(trans);
 			}
@@ -108,12 +91,12 @@ public class Ship extends TransformGroup implements GameListener {
 	}
 
 	@SuppressWarnings("unchecked")
-	private void getTransformsOfChildren(TransformGroup tg) {
-		Enumeration<Node> children = tg.getAllChildren();
+	private void getTransformsOfChildren(final TransformGroup tg) {
+		final Enumeration<Node> children = tg.getAllChildren();
 		while (children.hasMoreElements()) {
-			Node n = children.nextElement();
+			final Node n = children.nextElement();
 			if (n instanceof TransformGroup) {
-				Transform3D trans = new Transform3D();
+				final Transform3D trans = new Transform3D();
 				((TransformGroup) n).getTransform(trans);
 				this.staticTransforms.mul(trans);
 				getTransformsOfChildren((TransformGroup) n);
@@ -128,22 +111,21 @@ public class Ship extends TransformGroup implements GameListener {
 
 		final Transform3D rotY = new Transform3D();
 		rotY.rotY(Math.toRadians(180));
-		
+
 		final Transform3D scale = new Transform3D();
 		scale.setScale(1.5f);
-		
+
 		rotX.mul(rotY);
 		rotX.mul(scale);
 
-		TransformGroup rotateShip = new TransformGroup();
+		final TransformGroup rotateShip = new TransformGroup();
 		rotateShip.setTransform(rotX);
 
 		final BranchGroup ship = Java3DUtils.loadScene("/res/SFighter.obj");
-		ship.setCapability(BranchGroup.ALLOW_CHILDREN_WRITE);
-		
-		ship.setCollisionBounds(new BoundingBox(new Point3d(-0.35f, -0.15f,
-				-0.5f), new Point3d(0.35f, 0.06f, 0.5f)));
-		
+		ship.setCapability(Group.ALLOW_CHILDREN_WRITE);
+
+		ship.setCollisionBounds(new BoundingBox(new Point3d(-0.35f, -0.15f, -0.5f), new Point3d(0.35f, 0.06f, 0.5f)));
+
 		shape1 = (Shape3D) ship.getChild(0);
 		shape1.setCapability(Shape3D.ALLOW_APPEARANCE_WRITE);
 		shape1.getAppearance().setCapability(Appearance.ALLOW_TEXTURE_WRITE);
@@ -153,7 +135,7 @@ public class Ship extends TransformGroup implements GameListener {
 		shape2.setCapability(Shape3D.ALLOW_APPEARANCE_WRITE);
 		shape2.getAppearance().setCapability(Appearance.ALLOW_TEXTURE_WRITE);
 		shape2.getAppearance().setCapability(Appearance.ALLOW_MATERIAL_WRITE);
-		Material cockpit = new Material();
+		final Material cockpit = new Material();
 		cockpit.setAmbientColor(new Color3f(Color.CYAN));
 		cockpit.setDiffuseColor(new Color3f(Color.BLUE));
 		shape1.getAppearance().setMaterial(cockpit);
@@ -171,15 +153,15 @@ public class Ship extends TransformGroup implements GameListener {
 	public void added(final GameLogic game) {
 		// empty
 	}
-	
+
 	@Override
 	public void removed(final GameLogic game) {
 		// empty
 	}
-	
+
 	@Override
-	public void collided(GameLogic logic, Item item) {
-		Material mat = new Material();
+	public void collided(final GameLogic logic, final Item item) {
+		final Material mat = new Material();
 
 		if (item == Item.ASTEROID) {
 			mat.setAmbientColor(new Color3f(Color.RED));
@@ -195,36 +177,26 @@ public class Ship extends TransformGroup implements GameListener {
 	}
 
 	@Override
-	public void gamePaused(GameLogic game) {
-		// TODO Auto-generated method stub
-
+	public void gamePaused(final GameLogic game) {
+		// empty
 	}
 
 	@Override
-	public void gameResumed(GameLogic game) {
-		// TODO Auto-generated method stub
-
+	public void gameResumed(final GameLogic game) {
+		// empty
 	}
 
 	@Override
-	public void gameStarted(GameLogic game) {
-		// TODO Auto-generated method stub
+	public void gameStarted(final GameLogic game) {
+		// empty
 	}
 
 	@Override
-	public void gameStopped(GameLogic game) {
+	public void gameStopped(final GameLogic game) {
 		shipNavigationBehavior.reset();
 	}
-	
-//	@Override
-//	public void sessionFinished(GameLogic game) {
-//		// empty		
-//	}
 
 	private class FlashTimerTask extends TimerTask {
-
-		public FlashTimerTask() {
-		}
 
 		@Override
 		public void run() {
