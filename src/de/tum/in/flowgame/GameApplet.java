9f15f1b33c88660cd761b349189d6a3460d46046
google@vxart.de <google@vxart.de@1012749a-39bf-11de-9d05-31133e6853b1>
2009-10-18 13:05:18 +0000

    cleaned up imports

--- src/de/tum/in/flowgame/GameApplet.java
+++ src/de/tum/in/flowgame/GameApplet.java
@@ -5,8 +5,6 @@ import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.Frame;
 import java.awt.Toolkit;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.io.IOException;
