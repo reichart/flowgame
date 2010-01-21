71e92c4fd4702645fe8ca179a55217f66e1e330c
huebnerc <huebnerc@1012749a-39bf-11de-9d05-31133e6853b1>
2010-01-21 19:12:09 +0000

    changed colors, font size and alignment of the highscore table

--- trunk/src/de/tum/in/flowgame/ui/screens/HighscoresScreen.java
+++ trunk/src/de/tum/in/flowgame/ui/screens/HighscoresScreen.java
@@ -1,19 +1,41 @@
 package de.tum.in.flowgame.ui.screens;
 
+import java.awt.Color;
+import java.awt.Component;
 import java.awt.Container;
 import java.util.Random;
 
 import javax.swing.JButton;
+import javax.swing.JLabel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
+import javax.swing.SwingConstants;
+import javax.swing.table.DefaultTableCellRenderer;
+import javax.swing.table.TableCellRenderer;
 
 import de.tum.in.flowgame.ui.GameMenu;
 
 public class HighscoresScreen extends MenuScreen {
+	
+	/*
+	 * * Center the text in Table Cells
+	 */
+	private class CenterRenderer extends DefaultTableCellRenderer {
+		public CenterRenderer() {
+			setHorizontalAlignment(SwingConstants.CENTER);
+		}
 
+		public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
+			super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
+			return this;
+		}
+	}
+	
 	private final JButton back = goTo("Back", MainScreen.class);
 	
 	private final JScrollPane highscoresScroll;
+	
+	private JTable highscores;
 
 	public HighscoresScreen(final GameMenu menu) {
 		super(menu);
@@ -29,8 +51,14 @@ public class HighscoresScreen extends MenuScreen {
 				{"Pheobe", max - random.nextInt(max)}
 		};
 		final Object[] columnNames = {"Player", "Points"};
-		final JTable highscores = new JTable(rowData, columnNames);
+		highscores = new JTable(rowData, columnNames);
 		highscores.setEnabled(false);
+
+		CenterRenderer cr = new CenterRenderer();
+		for (int i=0; i < highscores.getColumnCount(); i++) {
+			highscores.getColumnModel().getColumn(i).setCellRenderer(cr);
+		}
+		
 		
 		highscoresScroll = new JScrollPane(highscores);
 	}
@@ -38,5 +66,10 @@ public class HighscoresScreen extends MenuScreen {
 	@Override
 	public Container getContents() {
 		return centered(title("Highscore"), highscoresScroll, back);
+	}
+	
+	@Override
+	public void update(){
+		System.out.println(highscores.getValueAt(0, 0));
 	}
 }
