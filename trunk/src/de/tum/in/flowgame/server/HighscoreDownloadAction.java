0e18b9051309921fde0a03e217bfc7920ac4f541
barbara.koehler <barbara.koehler@1012749a-39bf-11de-9d05-31133e6853b1>
2010-01-22 12:17:46 +0000

    highscore for player can be found, needed for social highscore

---
+++ trunk/src/de/tum/in/flowgame/server/HighscoreDownloadAction.java
@@ -0,0 +1,38 @@
+package de.tum.in.flowgame.server;
+
+import java.util.List;
+
+import de.tum.in.flowgame.dao.GameSessionDAOImpl;
+import de.tum.in.flowgame.dao.PersonDAO;
+import de.tum.in.flowgame.dao.PersonDAOImpl;
+import de.tum.in.flowgame.model.GameRound;
+import de.tum.in.flowgame.model.GameSession;
+import de.tum.in.flowgame.model.Person;
+
+public class HighscoreDownloadAction extends GameDataAction {
+
+	@Override
+	public Object execute(final Object id) throws Exception {
+		final PersonDAO persons = new PersonDAOImpl();
+		final Person person = persons.find((Long) id);
+		if (person == null) {
+			throw new NullPointerException("No person found for id " + id);
+		} else {
+			// TODO performance check
+			GameSessionDAOImpl gsdao = new GameSessionDAOImpl();
+			List<GameSession> gameSessions = gsdao.findAll();
+			Long highscore = Long.MIN_VALUE;
+			for (GameSession gameSession : gameSessions) {
+				if (gameSession.getPlayer().equals(person)) {
+					for (GameRound gameRound : gameSession.getRounds()) {
+						if (highscore < gameRound.getScore()) {
+							highscore = gameRound.getScore();
+						}
+					}
+				}
+			}
+			return highscore;
+		}
+	}
+
+}
