f40556a75420343f05aa6b838055a4deef25682d
huebnerc <huebnerc@1012749a-39bf-11de-9d05-31133e6853b1>
2010-02-16 09:23:11 +0000

    removed unnecessary import

--- src/de/tum/in/flowgame/ui/screens/HighscoresChartScreen.java
+++ src/de/tum/in/flowgame/ui/screens/HighscoresChartScreen.java
@@ -1,35 +1,18 @@
 package de.tum.in.flowgame.ui.screens;
 
-import java.awt.Component;
 import java.awt.Container;
-import java.awt.Graphics;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.Iterator;
-import java.util.List;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.JScrollPane;
-import javax.swing.JTable;
-import javax.swing.SwingConstants;
-import javax.swing.table.DefaultTableCellRenderer;
-import javax.swing.table.TableColumnModel;
-import javax.swing.table.TableModel;
 
-import com.lowagie.text.Image;
-
-import de.tum.in.flowgame.dao.GameSessionDAOImpl;
-import de.tum.in.flowgame.model.Score;
 import de.tum.in.flowgame.ui.GameMenu;
 
 public class HighscoresChartScreen extends MenuScreen {
 	
-    private ImageIcon imageIcon = new ImageIcon(new BufferedImage(500, 300, Image.ORIGINAL_PNG));
+    private ImageIcon imageIcon = new ImageIcon(new BufferedImage(500, 300, BufferedImage.TYPE_4BYTE_ABGR));
     private JLabel imageLabel = new JLabel(imageIcon);
 	
 	private final JButton back = goTo("Back", MainScreen.class);
