a75b2fc237c3dda5e2ef36eb452c23dc53c4a22b
philipp.reichart@gmx.de <philipp.reichart@gmx.de@1012749a-39bf-11de-9d05-31133e6853b1>
2010-03-07 14:04:04 +0000

    - formatting - removed @Override (we're compiling against 1.5 for MacOSX)

--- trunk/src/strategy/FunctionStrategy.java
+++ trunk/src/strategy/FunctionStrategy.java
@@ -3,20 +3,17 @@ package strategy;
 import de.tum.in.flowgame.GameLogic;
 import de.tum.in.flowgame.model.Function;
 
-public class FunctionStrategy implements FlowStrategy{
-	
-	private GameLogic logic;
-	private Function function;
-	
-	public FunctionStrategy(GameLogic logic, Function function){
+public class FunctionStrategy implements FlowStrategy {
+
+	private final GameLogic logic;
+	private final Function function;
+
+	public FunctionStrategy(final GameLogic logic, final Function function) {
 		this.function = function;
 		this.logic = logic;
 	}
-	
-	
 
-	@Override
-	public double calculateSpeed(double trendRating, double speed) {
+	public double calculateSpeed(final double trendRating, double speed) {
 		speed = -function.getValue(logic.getPosition());
 		if (speed > -30D) {
 			speed = -30D;
