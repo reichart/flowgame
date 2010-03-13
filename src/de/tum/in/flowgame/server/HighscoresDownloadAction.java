da4ff84e3038f873c010953dfe4c3cf7c7f34b8e
philipp.reichart@gmx.de <philipp.reichart@gmx.de@1012749a-39bf-11de-9d05-31133e6853b1>
2010-03-13 15:17:47 +0000

    added type parameters

--- src/de/tum/in/flowgame/server/HighscoresDownloadAction.java
+++ src/de/tum/in/flowgame/server/HighscoresDownloadAction.java
@@ -6,17 +6,12 @@ import de.tum.in.flowgame.dao.PersonDAO;
 import de.tum.in.flowgame.dao.PersonDAOImpl;
 import de.tum.in.flowgame.model.Highscore;
 
-public class HighscoresDownloadAction extends GameDataAction {
+public class HighscoresDownloadAction extends GameDataAction<List<Long>, List<Highscore>> {
 
 	@Override
-	public Object execute(final Object persons) throws Exception {
+	public List<Highscore> execute(final List<Long> persons) throws Exception {
 		final PersonDAO personDAO = new PersonDAOImpl();
-		if (persons instanceof List<?>) {
-			List<Highscore> highscores = personDAO.getHighscores((List<Long>) persons);
-			return highscores;
-		} else {
-			throw new Exception("The given parameters are not a list of person ids.");
-		}
+		return personDAO.getHighscores(persons);
 	}
 
 }
