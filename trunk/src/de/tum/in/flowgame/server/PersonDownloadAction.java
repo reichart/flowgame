57fcb2864df276ba63bf70618480db5a060ba2f5
barbara.koehler <barbara.koehler@1012749a-39bf-11de-9d05-31133e6853b1>
2010-03-15 21:58:00 +0000

    prevent lazily-loaded list on client side

--- trunk/src/de/tum/in/flowgame/server/PersonDownloadAction.java
+++ trunk/src/de/tum/in/flowgame/server/PersonDownloadAction.java
@@ -7,6 +7,10 @@ public class PersonDownloadAction extends GameDataAction<Long, Person> {
 	@Override
 	public Person execute(final Long id) throws Exception {
 		final Person person = em.find(Person.class, id);
+		
+		// prevent lazily-loaded list on client side
+		person.setProfilingAnswers(null);
+		
 		if (person == null) {
 			throw new NullPointerException("No person found for id " + id);
 		} else {
