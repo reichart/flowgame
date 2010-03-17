71cb07f45d0e8e595993395e2001c8ce94206ce6
philipp.reichart@gmx.de <philipp.reichart@gmx.de@1012749a-39bf-11de-9d05-31133e6853b1>
2010-03-17 23:18:34 +0000

    moved lazy-loading fix to avoid NPE

--- src/de/tum/in/flowgame/server/PersonDownloadAction.java
+++ src/de/tum/in/flowgame/server/PersonDownloadAction.java
@@ -7,13 +7,13 @@ public class PersonDownloadAction extends GameDataAction<Long, Person> {
 	@Override
 	public Person execute(final Long id) throws Exception {
 		final Person person = em.find(Person.class, id);
-		
-		// prevent lazily-loaded list on client side
-		person.setProfilingAnswers(null);
-		
+
 		if (person == null) {
 			throw new NullPointerException("No person found for id " + id);
 		} else {
+			// prevent lazily-loaded list on client side
+			person.setProfilingAnswers(null);
+
 			return person;
 		}
 	}
