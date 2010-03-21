3288433fde1a56044727f29aa4599c6793aca92c
barbara.koehler <barbara.koehler@1012749a-39bf-11de-9d05-31133e6853b1>
2010-03-21 20:31:53 +0000

    - moved Trend into de.tum.in.flowgame.strategy - moved Sounds into de.tum.in.flowgame - moved OALUtil into de.tum.in.flowgame.util

--- trunk/src/de/tum/in/flowgame/Sounds.java
+++ trunk/src/de/tum/in/flowgame/Sounds.java
@@ -1,7 +1,7 @@
-package de.tum.in.flowgame.engine;
+package de.tum.in.flowgame;
 
 import net.java.games.sound3d.Source;
-import de.tum.in.flowgame.engine.util.OALUtil;
+import de.tum.in.flowgame.util.OALUtil;
 
 public enum Sounds {
 
