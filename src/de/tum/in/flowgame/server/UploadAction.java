5127010fa7e1d5352edbc2330e5542544a6fc9b5
barbara.koehler <barbara.koehler@1012749a-39bf-11de-9d05-31133e6853b1>
2010-03-22 22:39:30 +0000

    commit uploaded data to database

--- src/de/tum/in/flowgame/server/UploadAction.java
+++ src/de/tum/in/flowgame/server/UploadAction.java
@@ -9,15 +9,21 @@ public class UploadAction extends GameDataAction<Object, Object> {
 	public Object execute(final Object o) throws Exception {
 		if (o instanceof Person) {
 			final Person person = (Person) o;
-			em.merge(person);
+			update(person);
 		} else if (o instanceof GameSession) {
 			final GameSession gameSession = (GameSession) o;
-			em.merge(gameSession);
+			update(gameSession);
 		} else {
 			throw new IllegalArgumentException("Invalid content type: " + o.getClass().getName());
 		}
 
 		return null;
+	}
+
+	private void update(final Object entity) {
+		em.getTransaction().begin();
+		em.merge(entity);
+		em.getTransaction().commit();
 	}
 
 }
