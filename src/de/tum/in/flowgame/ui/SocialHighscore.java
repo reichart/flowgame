856a8570f7d7cc06bd741684ac3420131feaab59
philipp.reichart@gmx.de <philipp.reichart@gmx.de@1012749a-39bf-11de-9d05-31133e6853b1>
2010-03-31 12:24:43 +0000

    + antialias lines

--- src/de/tum/in/flowgame/ui/SocialHighscore.java
+++ src/de/tum/in/flowgame/ui/SocialHighscore.java
@@ -4,6 +4,7 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.RenderingHints;
 import java.awt.geom.CubicCurve2D;
 import java.util.Iterator;
 import java.util.List;
@@ -69,6 +70,9 @@ public class SocialHighscore extends JPanel {
 		//Important: clears component background
 		super.paintComponent(g);
 
+		Graphics2D g2 = (Graphics2D) g;
+		g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+		
 		int lowerBorder = getHeight() - fb.getHeight();
 		int space = lowerBorder - 100 - 20;
 		int barBorder = lowerBorder - space;
