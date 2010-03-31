906416c0e2e24a49ce4bd8c9af9e0c8162801c12
barbara.koehler <barbara.koehler@1012749a-39bf-11de-9d05-31133e6853b1>
2010-03-31 10:51:36 +0000

    speedChangeBehavior pauses with rest of the game

--- trunk/src/de/tum/in/flowgame/engine/behavior/SpeedChangeBehavior.java
+++ trunk/src/de/tum/in/flowgame/engine/behavior/SpeedChangeBehavior.java
@@ -11,61 +11,89 @@ import de.tum.in.flowgame.GameLogic;
 import de.tum.in.flowgame.GameLogicConsumer;
 import de.tum.in.flowgame.Utils;
 import de.tum.in.flowgame.model.Function;
-import de.tum.in.flowgame.strategy.AverageTrendStrategy;
 import de.tum.in.flowgame.strategy.FlowStrategy;
 import de.tum.in.flowgame.strategy.FunctionStrategy;
+import de.tum.in.flowgame.strategy.FunctionStrategy2;
 
 public class SpeedChangeBehavior extends Behavior implements GameLogicConsumer, SpeedChangeBehaviorMBean {
 
 	private final WakeupCriterion newFrame = new WakeupOnElapsedFrames(0);
 	private final ShipNavigationBehavior forwardNavigator;
 	private double speed;
+	private double maxSpeed;
 	private GameLogic gameLogic;
-	private FlowStrategy strategy;
+	private final FlowStrategy strategy;
+
+	private boolean pause;
 
 	public SpeedChangeBehavior(final ShipNavigationBehavior forwardNavigator) {
 		this.forwardNavigator = forwardNavigator;
+		this.strategy = new FunctionStrategy2();
+		pause = false;
 		Utils.export(this);
-
 	}
 
 	@Override
 	public void initialize() {
 		this.wakeupOn(newFrame);
-		this.strategy = new AverageTrendStrategy();
-		// this.strategy = new FunctionStrategy();
 	}
 
 	@Override
 	@SuppressWarnings("unchecked")
 	public void processStimulus(final Enumeration criteria) {
-		if (this.strategy instanceof FunctionStrategy && strategy.getFunction() == null) {
-			strategy.setFunction(gameLogic.getDifficultyFunction().getSpeed());
-		}
-
-		if (gameLogic != null) {
-			final Function fun = gameLogic.getDifficultyFunction().getSpeed();
-			if (gameLogic.getCurrentScenarioRound().isBaselineRound()) {
-				speed = strategy.calculateSpeed(gameLogic.getAsteroidTrend(), gameLogic.getFuelTrend(), speed);
-			} else {
-				speed = (fun == null) ? 0 : -fun.getValue(gameLogic.getElapsedTime());
+		if (!pause) {
+			if ((this.strategy instanceof FunctionStrategy || this.strategy instanceof FunctionStrategy2)
+					&& strategy.getFunction() == null) {
+				strategy.setFunction(gameLogic.getDifficultyFunction().getSpeed());
 			}
 
-			forwardNavigator.setFwdSpeed(speed);
+			if (gameLogic != null) {
+				final Function fun = gameLogic.getDifficultyFunction().getSpeed();
+				if (gameLogic.getCurrentScenarioRound().isBaselineRound()) {
+					speed = strategy.calculateSpeed(gameLogic.getAsteroidTrend(), gameLogic.getFuelTrend(), speed);
+				} else {
+					speed = (fun == null) ? 0 : -fun.getValue(gameLogic.getElapsedTime());
+				}
+
+				if (speed > maxSpeed) {
+					maxSpeed = speed;
+				}
+
+				forwardNavigator.setFwdSpeed(speed);
+			}
+			wakeupOn(newFrame);
 		}
-		wakeupOn(newFrame);
 	}
 
 	public void setGameLogic(final GameLogic gameLogic) {
 		this.gameLogic = gameLogic;
-		this.gameLogic.addListener(new DefaultGameListener() {
-			@Override
-			public void gameStarted(GameLogic game) {
-				if (strategy != null) {
-					strategy.reset();
+		if (gameLogic != null) {
+			this.gameLogic.addListener(new DefaultGameListener() {
+				@Override
+				public void gameStarted(GameLogic game) {
+					if (strategy != null) {
+						strategy.reset();
+					}
+					pause = false;
 				}
-			}
-		});
+
+				@Override
+				public void gamePaused(GameLogic game) {
+					pause = true;
+				}
+
+				@Override
+				public void gameResumed(GameLogic game) {
+					pause = false;
+				}
+
+				@Override
+				public void gameStopped(GameLogic game) {
+					//game.removeListener(this);
+					double baseline = maxSpeed * 0.9;
+				}
+			});
+		}
 	}
 
 	public double getSpeed() {
