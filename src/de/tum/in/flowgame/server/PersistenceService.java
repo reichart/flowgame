a6593f84eed040bf46fb3e5fa6dbd8569fa14fcc
philipp.reichart@gmx.de <philipp.reichart@gmx.de@1012749a-39bf-11de-9d05-31133e6853b1>
2010-03-31 11:15:14 +0000

    log and rethrow exception when creating entity manager

--- src/de/tum/in/flowgame/server/PersistenceService.java
+++ src/de/tum/in/flowgame/server/PersistenceService.java
@@ -3,15 +3,25 @@ package de.tum.in.flowgame.server;
 import javax.persistence.EntityManager;
 import javax.persistence.Persistence;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
 import com.opensymphony.xwork2.inject.Inject;
 
 public class PersistenceService {
 
+	private final static Log log = LogFactory.getLog(PersistenceService.class);
+	
 	private final EntityManager em;
 
 	@Inject("persistenceUnit")
 	public PersistenceService(final String persistenceUnit) {
-		this.em = Persistence.createEntityManagerFactory(persistenceUnit).createEntityManager();
+		try {
+			this.em = Persistence.createEntityManagerFactory(persistenceUnit).createEntityManager();
+		} catch (final RuntimeException ex) {
+			log.error("failed to create entity manager", ex);
+			throw ex;
+		}
 	}
 
 	public EntityManager getEntityManager() {
