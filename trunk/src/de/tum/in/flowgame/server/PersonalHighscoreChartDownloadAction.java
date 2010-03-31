4b6eae33674f375def2cbb6b299bab8d1180cd6d
philipp.reichart@gmx.de <philipp.reichart@gmx.de@1012749a-39bf-11de-9d05-31133e6853b1>
2010-03-31 12:12:48 +0000

    + beautify chart highscore

--- trunk/src/de/tum/in/flowgame/server/PersonalHighscoreChartDownloadAction.java
+++ trunk/src/de/tum/in/flowgame/server/PersonalHighscoreChartDownloadAction.java
@@ -22,20 +22,23 @@ import org.jfree.data.xy.XYSeries;
 
 import de.tum.in.flowgame.model.GameRound;
 import de.tum.in.flowgame.model.GameSession;
-import de.tum.in.flowgame.model.Person;
 import de.tum.in.flowgame.model.Score;
 
 public class PersonalHighscoreChartDownloadAction extends DatabaseAction {
+
+	private final static Color TRANSPARENT = new Color(0, 0, 0, 0);
+	private final static Color TRANSPARENT_WHITE = new Color(1f, 1f, 1f, 0.75f);
 
 	private InputStream response;
 	private long personId;
 
 	@Override
 	public String execute() throws Exception {
-		final Person player = em.find(Person.class, personId);
 		final SortedSet<Score> scores = getPersonalScores(personId);
 		
-		final JFreeChart chart = new JFreeChart(player.getName(), JFreeChart.DEFAULT_TITLE_FONT, createPlot(createSeries(scores, player)), false);
+		final JFreeChart chart = new JFreeChart(null, JFreeChart.DEFAULT_TITLE_FONT, createPlot(createSeries(scores)), false);
+		chart.setBackgroundPaint(TRANSPARENT);
+		
 		final ByteArrayOutputStream bos = new ByteArrayOutputStream();
 		ImageIO.write(chart.createBufferedImage(500, 300), "png", bos);
 		this.response = new ByteArrayInputStream(bos.toByteArray());
@@ -43,8 +46,8 @@ public class PersonalHighscoreChartDownloadAction extends DatabaseAction {
 		return SUCCESS;
 	}
 
-	private static DefaultTableXYDataset createSeries(final SortedSet<Score> scores, final Person player) {
-		final XYSeries series = new XYSeries(player.getName(), false, false);
+	private static DefaultTableXYDataset createSeries(final SortedSet<Score> scores) {
+		final XYSeries series = new XYSeries("", false, false);
 		int i = 0;
 		for (final Score s : scores) {
 			series.add(i++, s.getScore());
@@ -56,18 +59,27 @@ public class PersonalHighscoreChartDownloadAction extends DatabaseAction {
 	}
 
 	private static XYPlot createPlot(final DefaultTableXYDataset xyd) {
-		final NumberAxis xAxis = new NumberAxis("Game");
-		xAxis.setAutoRangeIncludesZero(false);
-		final NumberAxis yAxis = new NumberAxis("Points");
+		final NumberAxis xAxis = beautifyAxis(new NumberAxis("Game"));
+		xAxis.setAutoTickUnitSelection(false);
+		
+		final NumberAxis yAxis = beautifyAxis(new NumberAxis("Points"));
+		
 		final XYItemRenderer renderer = new XYLineAndShapeRenderer(true, false);
-		// XYSplineRenderer renderer = new XYSplineRenderer();
-		// renderer.setSeriesShapesVisible(0, false);
+//		XYSplineRenderer renderer = new XYSplineRenderer();
+//		renderer.setSeriesShapesVisible(0, false);
 		renderer.setBaseOutlinePaint(Color.BLUE);
 		renderer.setSeriesStroke(0, new BasicStroke(3f));
 		renderer.setSeriesPaint(0, Color.BLUE);
 		final XYPlot plot = new XYPlot(xyd, xAxis, yAxis, renderer);
 		plot.setOrientation(PlotOrientation.VERTICAL);
 		return plot;
+	}
+
+	private static NumberAxis beautifyAxis(final NumberAxis axis) {
+		axis.setLabelPaint(TRANSPARENT_WHITE); // "Game"
+		axis.setTickLabelPaint(TRANSPARENT_WHITE); // 1,2,3,...
+		axis.setTickMarkPaint(TRANSPARENT_WHITE); // the tiny marks above the 1,2,3,...
+		return axis;
 	}
 
 	public SortedSet<Score> getPersonalScores(final long personId) {
