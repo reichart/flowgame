453b16934b166b1bf9d63da78f480a1a60dfcf10
barbara.koehler <barbara.koehler@1012749a-39bf-11de-9d05-31133e6853b1>
2010-05-16 16:06:14 +0000

    take steering settings from cookie

--- trunk/src/de/tum/in/flowgame/ui/screens/SettingsScreen.java
+++ trunk/src/de/tum/in/flowgame/ui/screens/SettingsScreen.java
@@ -14,22 +14,29 @@ import javax.swing.JCheckBox;
 import javax.swing.JRadioButton;
 import javax.swing.border.TitledBorder;
 
+import netscape.javascript.JSObject;
+
 import de.tum.in.flowgame.ui.GameMenu;
 
 public class SettingsScreen extends MenuScreen {
 
+	private static final String AIRPLANE = "airplane";
+	private static final String NORMAL = "normal";
+	private static final String STEERING = "steering";
 	private final Box steeringbox = new Box(BoxLayout.X_AXIS);
 	private final ButtonGroup steeringgroup = new ButtonGroup();
 
-	private final JRadioButton normal = new JRadioButton(new AbstractAction("normal") {
+	private final JRadioButton normal = new JRadioButton(new AbstractAction(NORMAL) {
 		public void actionPerformed(final ActionEvent e) {
 			menu.getGame().getShip().getControls().setNormalSteering(true);
+			setSteeringCookie(true);
 		}
 	});
 
-	private final JRadioButton airplane = new JRadioButton(new AbstractAction("airplane") {
+	private final JRadioButton airplane = new JRadioButton(new AbstractAction(AIRPLANE) {
 		public void actionPerformed(final ActionEvent e) {
 			menu.getGame().getShip().getControls().setNormalSteering(false);
+			setSteeringCookie(false);
 		}
 	});
 
@@ -38,15 +45,49 @@ public class SettingsScreen extends MenuScreen {
 			menu.getOverlay().setDrawFPS(fps.isSelected());
 		}
 	});
-	
+
 	private final JButton back = goTo("Back", MainScreen.class);
+
+	private void setSteeringCookie(boolean normalSteering) {
+		JSObject win = menu.getLogic().getWin();
+		if (win != null) {
+			Object[] newCookie = new Object[2];
+			newCookie[0] = STEERING;
+			if (normalSteering) {
+				newCookie[1] = NORMAL;
+				win.call("set_cookie", newCookie);
+			} else {
+				newCookie[1] = AIRPLANE;
+				win.call("set_cookie", newCookie);
+			}
+		}
+	}
 	
+	private void setSteering(boolean normalSteering) {
+		normal.setSelected(normalSteering);
+		menu.getGame().getShip().getControls().setNormalSteering(normalSteering);
+		airplane.setSelected(!normalSteering);
+	}
 
 	public SettingsScreen(final GameMenu menu) {
 		super(menu);
-		final boolean normalSteering = menu.getGame().getShip().getControls().getNormalSteering();
-		normal.setSelected(normalSteering);
-		airplane.setSelected(!normalSteering);
+
+		JSObject win = menu.getLogic().getWin();
+		if (win != null) {
+			Object[] cookieName = new Object[1];
+			cookieName[0] = STEERING;
+			String steering = (String) win.call("get_cookie", cookieName);
+			//cookie not set or set to a corrupt value
+			if (steering == null || !(steering.equals(NORMAL) || steering.equals(AIRPLANE))) {
+				setSteeringCookie(true);
+				setSteering(true);
+			} else if (steering.equals(NORMAL)) {
+				setSteering(true);
+			} else if (steering.equals(AIRPLANE)) {
+				setSteering(false);
+			}
+		}
+
 		steeringgroup.add(normal);
 		steeringgroup.add(airplane);
 		final TitledBorder steeringborder = BorderFactory.createTitledBorder("Steering");
