bad8aac52938817cf0ec4c481c7af2bca609175e
philipp.reichart@gmx.de <philipp.reichart@gmx.de@1012749a-39bf-11de-9d05-31133e6853b1>
2010-05-23 14:18:00 +0000

    catch exceptions when loading sound (mainly when not running as applet)

--- src/de/tum/in/flowgame/Sounds.java
+++ src/de/tum/in/flowgame/Sounds.java
@@ -2,15 +2,21 @@ package de.tum.in.flowgame;
 
 import java.applet.AudioClip;
 
+import org.apache.commons.logging.LogFactory;
+
 public enum Sounds {
 
 	ASTEROID("crash"), FUELCAN("water-droplet-1"), DEATH("chewie");
 
-	private final AudioClip snd;
+	private AudioClip snd;
 
 	private Sounds(final String res) {
-		this.snd = SoundsHelper.getApplet().getAudioClip(getClass().getResource("/res/sound/" + res + ".wav"));
-		
+		try {
+			this.snd = SoundsHelper.getApplet().getAudioClip(getClass().getResource("/res/sound/" + res + ".wav"));
+		} catch (final Exception ex) {
+			LogFactory.getLog(Sounds.class).warn("failed to load sound " + res, ex);
+			this.snd = null;
+		}
 	}
 
 	public void play() {
