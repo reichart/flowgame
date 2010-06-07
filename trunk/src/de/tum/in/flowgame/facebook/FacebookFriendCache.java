87b4f2a42d50e50c46b77d26afe38792fb2754b4
philipp.reichart@gmx.de <philipp.reichart@gmx.de@1012749a-39bf-11de-9d05-31133e6853b1>
2010-06-07 12:12:02 +0000

    improved offline support

--- trunk/src/de/tum/in/flowgame/facebook/FacebookFriendCache.java
+++ trunk/src/de/tum/in/flowgame/facebook/FacebookFriendCache.java
@@ -6,6 +6,8 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -17,6 +19,8 @@ import de.tum.in.flowgame.GameApplet;
 import de.tum.in.flowgame.Utils;
 
 public class FacebookFriendCache {
+	
+	private static final Log log = LogFactory.getLog(FacebookFriendCache.class);
 
 	/*
 	 * Dummy profile pic for users with no PIC_SQUARE, fall back to empty pic.
@@ -43,6 +47,11 @@ public class FacebookFriendCache {
 	}
 
 	public void updateFriends() throws FacebookException, JSONException {
+		if (facebook == null) {
+			log.warn("no facebook, not updating friends");
+			return;
+		}
+		
 		final JSONArray fbfriends = facebook.friends_get();
 
 		if (fbfriends == null || fbfriends.length() == 0) {
@@ -111,6 +120,11 @@ public class FacebookFriendCache {
 	}
 
 	public Friend getCurrentPlayer() throws Exception {
+		if (facebook == null) {
+			log.warn("no facebook, returning dummy user");
+			return new Friend(-1, "dummy", DUMMY_PROFILE_PIC);
+		}
+		
 		if (currentPlayer == null) {
 			long user = facebook.users_getLoggedInUser();
 			List<Long> users = new ArrayList<Long>();
