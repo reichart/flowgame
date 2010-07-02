dfe9484c480d4a1c2a21378ab7074c3dece0b880
philipp.reichart@gmx.de <philipp.reichart@gmx.de@1012749a-39bf-11de-9d05-31133e6853b1>
2010-07-02 14:20:46 +0000

    cleanup

--- src/de/tum/in/flowgame/server/ScenarioSessionDownloadAction.java
+++ src/de/tum/in/flowgame/server/ScenarioSessionDownloadAction.java
@@ -9,49 +9,47 @@ import java.util.Random;
 import de.tum.in.flowgame.model.ScenarioRound;
 import de.tum.in.flowgame.model.ScenarioSession;
 
-public class ScenarioSessionDownloadAction extends
-		GameDataAction<Long, ScenarioSession> {
+public class ScenarioSessionDownloadAction extends GameDataAction<Long, ScenarioSession> {
 
 	/**
-	 * Download one of the ScenarioSessions, which one is chosen randomly
-	 * the rounds within the Scenario are sorted in 50% of the cases otherwise they are shuffled (baseline rounds stay at the beginning)
+	 * Download one of the ScenarioSessions, which one is chosen randomly the
+	 * rounds within the Scenario are sorted in 50% of the cases otherwise they
+	 * are shuffled (baseline rounds stay at the beginning)
 	 */
 	@Override
 	public ScenarioSession execute(final Long playerId) throws Exception {
-		Random rnd = new Random(System.currentTimeMillis());
-		boolean sort = rnd.nextBoolean();
-		System.err.println("sorted " + sort);
+		final Random rnd = new Random(System.currentTimeMillis());
+		final boolean sort = rnd.nextBoolean();
 
 		final List<ScenarioSession> scenarioSessions = list(ScenarioSession.class);
 		final Integer id = rnd.nextInt(scenarioSessions.size());
-		System.err.println(id);
+		
 		ScenarioSession session = null;
 		if (id < scenarioSessions.size()) {
 			session = scenarioSessions.get(id);
-			List<ScenarioRound> rounds = session.getRounds();
+			final List<ScenarioRound> rounds = session.getRounds();
 			if (sort) {
 				Collections.sort(rounds);
 			} else {
-				//Make sure that baseline rounds are not shuffled
-				//remove them from list
-				List<ScenarioRound> baselineRounds = new ArrayList<ScenarioRound>();
-				for (Iterator iterator = rounds.iterator(); iterator.hasNext();) {
-					ScenarioRound round = (ScenarioRound) iterator.next();
+				// Make sure that baseline rounds are not shuffled
+				// remove them from list
+				final List<ScenarioRound> baselineRounds = new ArrayList<ScenarioRound>();
+				for (final Iterator<ScenarioRound> iterator = rounds.iterator(); iterator.hasNext();) {
+					final ScenarioRound round = iterator.next();
 					if (round.isBaselineRound()) {
 						baselineRounds.add(round);
 						iterator.remove();
 					}
-					
+
 				}
 				Collections.shuffle(rounds);
-				//readd baseline Rounds at the beginning of the rounds list
+				// readd baseline Rounds at the beginning of the rounds list
 				rounds.addAll(0, baselineRounds);
 			}
 		}
 
 		if (session == null) {
-			throw new Exception("No valid session could be found for player "
-					+ playerId);
+			throw new Exception("No valid session could be found for player " + playerId);
 		} else {
 			return session;
 		}
