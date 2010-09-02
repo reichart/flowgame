4d78f4fc4c5e8b4144a0556be0dc86203c5e130c
barbara.koehler <barbara.koehler@1012749a-39bf-11de-9d05-31133e6853b1>
2010-09-02 15:06:27 +0000

    story displayed before and after each round exept on SessionIntroScreen

--- src/de/tum/in/flowgame/client/ui/screens/story/SessionIntroScreen.java
+++ src/de/tum/in/flowgame/client/ui/screens/story/SessionIntroScreen.java
@@ -4,20 +4,33 @@ import java.awt.Container;
 
 import javax.swing.JButton;
 
+import de.tum.in.flowgame.GameLogic;
 import de.tum.in.flowgame.client.ui.screens.BeforeSessionQuestionnaireScreen;
-import de.tum.in.flowgame.client.ui.screens.MenuScreen;
 import de.tum.in.flowgame.client.ui.screens.UIMessages;
 
 /**
  * Displayed when going from the main menu to the initial session questionnaire.
  */
-public class SessionIntroScreen extends MenuScreen {
+public class SessionIntroScreen extends StoryScreen {
 
 	private final JButton next = goTo(UIMessages.CONTINUE, BeforeSessionQuestionnaireScreen.class);
-
+	
+	@Override
+	public void update(final GameLogic logic) throws Exception {
+		super.update(logic);
+		title(UIMessages.getString("scenario" + menu.getLogic().getStoryScenario() + ".name"));
+		repaint();
+	}
+	
 	@Override
 	public Container getContents() {
-		return centered(title(UIMessages.getString("session.intro")), next);
+		return centered(COMMON_BORDER, title("lala"), text, next);
 	}
 
+	@Override
+	protected String getText() {
+		final int storyScenario = menu.getLogic().getStoryScenario();
+		return UIMessages.getString("scenario" + storyScenario + ".intro");
+	}
+	
 }
