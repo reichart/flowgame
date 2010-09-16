b5b70fcad29705877a0844276f7a5216c90a8df4
philipp.reichart@gmx.de <philipp.reichart@gmx.de@1012749a-39bf-11de-9d05-31133e6853b1>
2010-09-16 12:26:47 +0000

    protect against null input

--- src/de/tum/in/flowgame/client/facebook/JSONUtils.java
+++ src/de/tum/in/flowgame/client/facebook/JSONUtils.java
@@ -9,6 +9,10 @@ import org.json.JSONException;
 public class JSONUtils {
 
 	public static Set<Long> toLongs(final JSONArray array) throws JSONException {
+		if (array == null) {
+			return null;
+		}
+		
 		final Set<Long> result = new HashSet<Long>();
 		for (int i = 0; i < array.length(); i++) {
 			result.add(array.getLong(i));
