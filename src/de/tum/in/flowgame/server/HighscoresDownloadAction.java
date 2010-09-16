fd8d05f37f142bf85b9c7cd14adcbe1c1004fd20
barbara.koehler <barbara.koehler@1012749a-39bf-11de-9d05-31133e6853b1>
2010-09-16 11:42:28 +0000

    avoid querying database when no friends play flowspace

--- src/de/tum/in/flowgame/server/HighscoresDownloadAction.java
+++ src/de/tum/in/flowgame/server/HighscoresDownloadAction.java
@@ -9,20 +9,22 @@ public class HighscoresDownloadAction extends GameDataAction<List<Long>, List<Hi
 
 	@Override
 	public List<Highscore> execute(final List<Long> persons) throws Exception {
-		final List<Highscore> globalHighscore = getGlobalHighscore();
-		final List<Highscore> personsHighscore = getPersonsHighscore(persons);
-
 		final List<Highscore> friendsHighscore = new ArrayList<Highscore>();
 
-		for (final Highscore highscore : personsHighscore) {
-			// if you are the only person to play
-			final Integer percentage;
-			if (globalHighscore.size() <= 1) {
-				percentage = 100;
-			} else {
-				percentage = (int) (globalHighscore.indexOf(highscore) * 100.0 / (globalHighscore.size() - 1));
+		if (persons != null && !persons.isEmpty()) {
+			final List<Highscore> globalHighscore = getGlobalHighscore();
+			final List<Highscore> personsHighscore = getPersonsHighscore(persons);
+
+			for (final Highscore highscore : personsHighscore) {
+				// if you are the only person to play
+				final Integer percentage;
+				if (globalHighscore.size() <= 1) {
+					percentage = 100;
+				} else {
+					percentage = (int) (globalHighscore.indexOf(highscore) * 100.0 / (globalHighscore.size() - 1));
+				}
+				friendsHighscore.add(new Highscore(highscore.getPersonid(), highscore.getScore(), percentage));
 			}
-			friendsHighscore.add(new Highscore(highscore.getPersonid(), highscore.getScore(), percentage));
 		}
 
 		return friendsHighscore;
@@ -30,30 +32,24 @@ public class HighscoresDownloadAction extends GameDataAction<List<Long>, List<Hi
 
 	@SuppressWarnings("unchecked")
 	private List<Highscore> getGlobalHighscore() {
-		return transform(em.createQuery("SELECT p.id, MAX(r.score) AS highscore" +
-				" FROM Person p, GameSession s, IN (s.rounds) r" +
-				" WHERE s.player.id = p.id" + 
-				" GROUP BY p.id" + 
-				" ORDER BY highscore")
-				.getResultList());
+		return transform(em.createQuery(
+				"SELECT p.id, MAX(r.score) AS highscore" + " FROM Person p, GameSession s, IN (s.rounds) r" + " WHERE s.player.id = p.id"
+						+ " GROUP BY p.id" + " ORDER BY highscore").getResultList());
 	}
-	
+
 	@SuppressWarnings("unchecked")
 	private List<Highscore> getPersonsHighscore(final List<Long> persons) {
-		return transform(em.createQuery("SELECT p.id, MAX(r.score) AS highscore" +
-				" FROM Person p, GameSession s, IN (s.rounds) r" +
-				" WHERE s.player.id = p.id" + 
-				" AND p.id IN :personids" +
-				" GROUP BY p.id" + 
-				" ORDER BY highscore")
-			.setParameter("personids", persons)
-			.getResultList());
+		return transform(em
+				.createQuery(
+						"SELECT p.id, MAX(r.score) AS highscore" + " FROM Person p, GameSession s, IN (s.rounds) r" + " WHERE s.player.id = p.id"
+								+ " AND p.id IN :personids" + " GROUP BY p.id" + " ORDER BY highscore").setParameter("personids", persons)
+				.getResultList());
 	}
-	
+
 	private List<Highscore> transform(final List<Object[]> highscore) {
 		final List<Highscore> list = new ArrayList<Highscore>();
 		for (final Object[] objects : highscore) {
-			list.add(new Highscore((Long) objects[0],(Long) objects[1]));
+			list.add(new Highscore((Long) objects[0], (Long) objects[1]));
 		}
 		return list;
 	}
